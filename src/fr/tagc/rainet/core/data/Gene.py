89998b04ad6624b7347edd45213b0b6d22f56dc3
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-01-28 09:56:22 +0000

    Added Gene table

---
+++ src/fr/tagc/rainet/core/data/Gene.py
@@ -0,0 +1,51 @@
+
+from sqlalchemy import Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+from fr.tagc.rainet.core.util.sql.Base import Base
+from fr.tagc.rainet.core.util.sql.SQLManager import SQLManager
+
+from fr.tagc.rainet.core.util.exception.RainetException import RainetException
+from fr.tagc.rainet.core.util.log.Logger import Logger
+
+from fr.tagc.rainet.core.data import DataConstants
+from fr.tagc.rainet.core.util.exception.NotRequiredInstantiationException import NotRequiredInstantiationException
+from sqlalchemy.orm.base import instance_dict
+
+
+# #
+# This class describes a Gene models obtained from querying Ensembl BioMart, to make correspondence between transcripts coming from the same Gene
+#
+class Gene( Base ):
+    __tablename__ = 'Gene'
+
+    # The Ensembl Gene ID
+    geneID = Column( String, primary_key = True )
+    # The base RNA
+    transcriptList = relationship( 'RNA' , backref="Gene")
+
+    # #
+    # The Gene constructor
+    # 
+    # @param gene_ID : string - The provided Gene ID
+    #### dr: NEED TO FILL documentation    
+    def __init__( self, gene_ID):
+        
+        self.geneID = gene_ID
+
+    # #
+    # Add a RNA transcript to the list
+    #
+    # @param rna : string - A RNA instance
+    def add_rna( self, rna ):
+
+        if rna != None:
+            self.transcriptList.append( rna )
+         
+    # #
+    # Add the object to SQLAlchemy session
+    def add_to_session( self ):
+    
+        sql_session = SQLManager.get_instance().get_session()
+        sql_session.add( self )
+        
