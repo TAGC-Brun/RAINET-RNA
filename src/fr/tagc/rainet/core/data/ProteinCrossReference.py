60fab9dd6c48ec67ff3a73db337f645c28216636
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-02-02 09:43:05 +0000

    changes from Lionel regarding protein IDs with "-"

--- src/fr/tagc/rainet/core/data/ProteinCrossReference.py
+++ src/fr/tagc/rainet/core/data/ProteinCrossReference.py
@@ -35,21 +35,30 @@ class ProteinCrossReference( Base ):
         
         # Get a SQLalchemy session
         sql_session = SQLManager.get_instance().get_session()
-        
-        # control if protein ACC contains a isoform code (like for instance P31496-1 instead of P31496)
-        index_dash = protein_acc.index("-")
-        if index_dash > 0:
+
+        # Control if protein ACC contains a isoform code (like for instance P31496-1 instead of P31496)
+        # If so, keep only the root name (the part before the "-") as protein ACC
+        try:
+            index_dash = protein_acc.index("-")
             protein_acc = protein_acc[0:index_dash]
+        except ValueError:
+            pass
         
         # Retrieve the list of Protein corresponding to the provided accession number
         from fr.tagc.rainet.core.data.Protein import Protein
         protein_list = sql_session.query( Protein).filter( or_( Protein.uniprotAC == protein_acc, Protein.uniprotID == protein_acc)).all()
 
-        # If a single protein exists with the given accession number, create the
-        # new ProteinCrossReferenceObject with the right value and and it to the Protein
+        # If a single protein exists with the given accession number, check if a similar
+        # cross reference exists in the database (may be true due to protein isoform uniprotACC which is cut) 
+        # If the cross reference does not exist, a new ProteinCrossReferenceObject with the right value and add it to the Protein
         # cross reference list
         if protein_list != None and len( protein_list) == 1 :
             protein = protein_list[0]
+            cross_reference_list = sql_session.query( ProteinCrossReference).filter( ProteinCrossReference.protein_id == protein.uniprotAC,
+                                                                                     ProteinCrossReference.sourceDB == db_source,
+                                                                                     ProteinCrossReference.crossReferenceID == cross_reference).all()
+            if cross_reference_list != None and len( cross_reference_list) > 0:
+                raise NotRequiredInstantiationException( "ProteinCrossReference.init : Cross Reference already present in database.")
             self.sourceDB = db_source
             self.crossReferenceID = cross_reference
             protein.add_cross_reference( self)
