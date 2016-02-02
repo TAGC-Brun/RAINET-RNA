da66bac32e9ab4dc874bcf4590494dbe18e226ca
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-02-02 09:54:22 +0000

    commiting mRNA table

--- branches/RNA_insertion/src/fr/tagc/rainet/core/data/MRNA.py
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/data/MRNA.py
@@ -2,15 +2,24 @@
 from sqlalchemy import Column, String, Integer, Boolean, Float, ForeignKey
 
 from fr.tagc.rainet.core.data.RNA import RNA
+from fr.tagc.rainet.core.data.Protein import Protein
+from fr.tagc.rainet.core.data.ProteinCrossReference import ProteinCrossReference
+
+from fr.tagc.rainet.core.util.sql.Base import Base
+from fr.tagc.rainet.core.util.sql.SQLManager import SQLManager
+
 
 class MRNA( RNA ):
     
     __tablename__ = 'MRNA'
     
     transcriptID = Column( String, ForeignKey('RNA.transcriptID'), primary_key=True)
-    
+
     __mapper_args__ = { 'polymorphic_identity':'MRNA'}
 
+
+
     def __init__( self ):
+        
         pass
 
