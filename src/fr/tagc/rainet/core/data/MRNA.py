92e9d1b836062b8b7dc141cd24e2de450440403a
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-02-08 10:42:24 +0000

    now inserting proteinID on mRNAs

--- src/fr/tagc/rainet/core/data/MRNA.py
+++ src/fr/tagc/rainet/core/data/MRNA.py
@@ -7,6 +7,8 @@ from fr.tagc.rainet.core.data.ProteinCrossReference import ProteinCrossReference
 
 from fr.tagc.rainet.core.util.sql.Base import Base
 from fr.tagc.rainet.core.util.sql.SQLManager import SQLManager
+from fr.tagc.rainet.core.data import DataConstants
+from fr.tagc.rainet.core.util.log.Logger import Logger
 
 
 class MRNA( RNA ):
@@ -15,11 +17,28 @@ class MRNA( RNA ):
     
     transcriptID = Column( String, ForeignKey('RNA.transcriptID'), primary_key=True)
 
+    # The Protein Uniprot_ac ID
+    proteinID = Column( String, ForeignKey( 'Protein.uniprotAC'))
+
+
     __mapper_args__ = { 'polymorphic_identity':'MRNA'}
 
 
 
-    def __init__( self ):
+    def __init__( self, peptide_id ):
         
-        pass
+        #=======================================================================
+        # Query Protein uniprot_ac using cross references
+        # From Cross ref ID, search primary ID using cross reference DataManager item created during insertion
+        # Note: assuming that if uniprot_ac exists in protein cross reference table it will also exist in protein table.
+        #=======================================================================
+ 
+        from fr.tagc.rainet.core.util.data.DataManager import DataManager
+
+        proteinXrefs = DataManager.get_instance().get_data(DataConstants.PROTEIN_RNA_INTERACTION_CATRAPID_PXREF)
+        
+        if peptide_id in proteinXrefs:
+            self.proteinID = proteinXrefs[peptide_id][0]
+        else:
+            Logger.get_instance().debug( "\nPeptide ID not found:\t" + str(peptide_id) )
 
