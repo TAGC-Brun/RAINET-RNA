9bdecf7681d0982217d44954f61514d2b1beb3b8
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-02-11 11:10:14 +0000

    changed gencode option to int, after Lionel fix on OptionManager

--- branches/RNA_insertion/src/fr/tagc/rainet/core/util/option/OptionConstants.py
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/util/option/OptionConstants.py
@@ -1,4 +1,5 @@
 from fr.tagc.rainet.core.util import Constants
+from fr.tagc.rainet.core.data import DataConstants
 
 #===============================================================================
 # The list of available strageties
@@ -24,8 +25,8 @@ OPTION_DB_NAME = "databasePath"
 OPTION_SPECIES = "species"
 OPTION_VERBOSITY = "verbose"
 # Insertion strategy options
-OPTION_INSERTION_PROPERTIES_PATH = "--insertionPropertiesPath (-i)"
-OPTION_INSERTION_FORCE_OVERRIDE = "--insertionForceOverride (-f)"
+OPTION_INSERTION_PROPERTIES_PATH = "insertionPropertiesPath"
+OPTION_INSERTION_FORCE_OVERRIDE = "insertionForceOverride"
 # InteractiveQuery Strategy options
 OPTION_QUERY_FILE = "query_file"
 # Analysis Strategy options
@@ -33,6 +34,22 @@ OPTION_MINIMUM_INTERACTION_SCORE = "interaction_score"
 OPTION_TRANSCRIPT_BIOTYPE = "transcript_biotype"
 OPTION_LNCRNA_BIOTYPES = "lncRNA_biotypes"
 OPTION_GENCODE = "gencode_basic"
+
+#===============================================================================
+# Constants for analysis strategy
+#===============================================================================
+
+DEFAULT_BIOTYPE = "RNA"
+DEFAULT_LNCRNA_BIOTYPES = "OFF"
+DEFAULT_INTERACTION_SCORE = "OFF"
+DEFAULT_GENCODE = 0
+
+BIOTYPE_LNCRNA = "LncRNA"
+BIOTYPE_MRNA = "MRNA"
+BIOTYPE_OTHERRNA = "OtherRNA"
+TRANSCRIPT_BIOTYPES = { DEFAULT_BIOTYPE, BIOTYPE_LNCRNA, BIOTYPE_MRNA, BIOTYPE_OTHERRNA }
+LNCRNA_BIOTYPES = DataConstants.RNA_LNCRNA_BIOTYPE
+
 
 #===============================================================================
 # The definition of the options
@@ -43,7 +60,6 @@ OPTION_LIST = {  "Insertion": [
                     [ "-v", "--verbose", "store", "string", OPTION_VERBOSITY, Constants.MODE_INFO, "The level of verbosity. Must be one of : " + str( Constants.VERBOSITY_LEVELS)],
                     [ "-i", "--insertionPropertiesPath", "store", "string", OPTION_INSERTION_PROPERTIES_PATH, None, "The path to the properties file containing the list of files to use for DB insertion."],
                     [ "-f", "--forceOverride", "store_true", None, OPTION_INSERTION_FORCE_OVERRIDE, None, "Indicates if the whole database must be dropped and re-inserted or not."],
-                    
                 ], 
                  "InteractiveQuery":[
                     [ "-d", "--databasePath", "store", "string", OPTION_DB_NAME, None, "The path to the SQL database to use/create."],
@@ -54,10 +70,10 @@ OPTION_LIST = {  "Insertion": [
                     [ "-d", "--databasePath", "store", "string", OPTION_DB_NAME, None, "The path to the SQL database to use/create."],
                     [ "-s", "--species", "store", "string", OPTION_SPECIES, None, "The species used in the database."],
                     [ "-v", "--verbose", "store", "string", OPTION_VERBOSITY, Constants.MODE_INFO, "The level of verbosity. Must be one of : " + str( Constants.VERBOSITY_LEVELS)],
-                    [ "-m", "--minimumInteractionScore", "store", "string", OPTION_MINIMUM_INTERACTION_SCORE, Constants.DEFAULT_INTERACTION_SCORE, "Protein-RNA interactions with interaction score below given value will be excluded from analysis. Default: OFF"],
-                    [ "-b", "--transcriptBiotype", "store", "string", OPTION_TRANSCRIPT_BIOTYPE, Constants.DEFAULT_BIOTYPE, "General transcript biotype to be INCLUDED in analysis. Default = RNA (i.e. any type of RNA). Can be one of :"+ str(Constants.TRANSCRIPT_BIOTYPES)],
-                    [ "-l", "--lncRNABiotype", "store", "string", OPTION_LNCRNA_BIOTYPES, Constants.DEFAULT_LNCRNA_BIOTYPES, "Comma-separated list of lncRNA subtypes to be INCLUDED in analysis. Default: OFF (i.e. all subtypes are considered). Can one or several of :"+ str(Constants.LNCRNA_BIOTYPES)],
-                    [ "-g", "--gencodeBasicOnly", "store", "int", OPTION_GENCODE, Constants.DEFAULT_GENCODE, "If 1, include in analysis ONLY transcripts tagged as present in 'GENCODE basic'. Default: OFF (i.e. all RNAs are considered)."]
+                    [ "-m", "--minimumInteractionScore", "store", "string", OPTION_MINIMUM_INTERACTION_SCORE, DEFAULT_INTERACTION_SCORE, "Protein-RNA interactions with interaction score below given value will be excluded from analysis. Default: OFF"],
+                    [ "-b", "--transcriptBiotype", "store", "string", OPTION_TRANSCRIPT_BIOTYPE, DEFAULT_BIOTYPE, "General transcript biotype to be INCLUDED in analysis. Default = RNA (i.e. any type of RNA). Can be one of :"+ str(TRANSCRIPT_BIOTYPES)],
+                    [ "-l", "--lncRNABiotype", "store", "string", OPTION_LNCRNA_BIOTYPES, DEFAULT_LNCRNA_BIOTYPES, "Comma-separated list of lncRNA subtypes to be INCLUDED in analysis. Default: OFF (i.e. all subtypes are considered). Can one or several of :"+ str(LNCRNA_BIOTYPES)],
+                    [ "-g", "--gencodeBasicOnly", "store", "int", OPTION_GENCODE, DEFAULT_GENCODE, "If 1, include in analysis ONLY transcripts tagged as present in 'GENCODE basic'. Default: 0 (i.e. all RNAs are considered)."]
                 ],
                 "DatabaseCheck" : [
                     [ "-d", "--databasePath", "store", "string", OPTION_DB_NAME, None, "The path to the SQL database to use/create."],
