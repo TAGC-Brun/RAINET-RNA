8687ba25bdf62e2ac847345723b8dd84b36c5b0c
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-02-16 18:11:56 +0000

    added plot of distribution of example transcripts

--- src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/GTEx_statistics.R
+++ src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/GTEx_statistics.R
@@ -8,7 +8,7 @@ library(knitr)
 args <- commandArgs(TRUE)
 
 # Test if we have enough arguments
-if( length(args) != 4){
+if( length(args) != 5){
   stop("Bad argument number")
 }
 
@@ -16,5 +16,6 @@ working_dir = args[1]
 annotation_input_file = args[2]
 expression_input_file = args[3]
 expression_sample_input_file = args[4]
+tx_expression_input_folder = args[5]
 
 knit2pdf('GTEx_statistics.Rnw')
--- src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/density_plot_example_transcripts.R
+++ src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/density_plot_example_transcripts.R
@@ -2,12 +2,14 @@
 library(ggplot2)
 library(reshape)
 library(RColorBrewer)
-library(gridExtra)
+require(grid)
+require(gridExtra)
 
+#tx_expression_input_folder = "/home/diogo/testing/folder"
+#tx_expression_input_folder = "/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/processing/expressionData/dataFiles/transcript_expression"
 
-#From: https://github.com/hadley/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
-grid_arrange_shared_legend <- function(...) {
-  plots <- list(...)
+#EDITED From template: https://github.com/hadley/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
+grid_arrange_shared_legend <- function(plots) {
   g <- ggplotGrob(plots[[1]] + theme(legend.position="bottom"))$grobs
   legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
   lheight <- sum(legend$height)
@@ -19,26 +21,27 @@ grid_arrange_shared_legend <- function(...) {
     heights = unit.c(unit(1, "npc") - lheight, lheight))
 }
 
-expression_input_file = "/home/diogo/testing/expression_test.csv"
-#expression_input_file = "/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/processing/expressionData/dataFiles/expression.csv"
+listPlot = list()
+i = 0
+for (file in list.files(tx_expression_input_folder)){
+  expression_input_file = paste(tx_expression_input_folder,file,sep="/")
+  
+  nc <- max(count.fields(expression_input_file, sep=","))
+  expression_df <- read.table(expression_input_file, sep=",", row.names = 1, col.names=paste("V",1:nc,sep="."), fill=T)
+  expression_df <- as.data.frame(t(expression_df))
+  expression_df <- melt(expression_df)
+  colourCount = length(unique(expression_df$variable))
+  getPalette = colorRampPalette(brewer.pal(7, "Set1")) #this creates a function, which will take total number of shades of colours to use
+  
+  tissues = factor(expression_df$variable)
 
-nc <- max(count.fields(expression_input_file, sep=","))
-expression_df <- read.table(expression_input_file, sep=",", row.names = 1, col.names=paste("V",1:nc,sep="."), fill=T)
-expression_df <- as.data.frame(t(expression_df))
-expression_df <- melt(expression_df)
-colourCount = length(unique(expression_df$variable))
-getPalette = colorRampPalette(brewer.pal(7, "Set1")) #this creates a function, which will take total number of shades of colours to use
+  i=i+1
+  
+  listPlot[[i]] <- ggplot(expression_df, aes(x = value ) )  +
+    geom_density(aes(color=tissues), size=1) + # could also use fill instead of color
+    ggtitle(file) +
+    theme_minimal()
+}
 
-tissues = factor(expression_df$variable)
-
-plt1 <- ggplot(expression_df, aes(x = value ) )  +
-  geom_density(aes(color=tissues), size=3) +
-  theme_minimal()
-
-plt2 <- ggplot(expression_df, aes(x = value ) )  +
-  geom_density(aes(fill=factor(variable)), size=3) +
-  theme_minimal()
-
-grid_arrange_shared_legend(plt1,plt2)
-
+grid_arrange_shared_legend(listPlot)
 
