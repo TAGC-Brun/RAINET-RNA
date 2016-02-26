deb30260bed9da95ba0874a5142686752a5a6c87
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-02-26 16:12:44 +0000

    added folder and scripts that will contain R scripts to produce a pdf report from the analysisStrategy

---
+++ src/fr/tagc/rainet/core/execution/analysis/Rscripts/expression_comparison.R
@@ -0,0 +1,20 @@
+
+library(ggplot2)
+library(reshape)
+library(RColorBrewer)
+
+inFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_results/Report/rna_expression.tsv"
+
+dataset = read.table( inFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
+
+plt1 <- ggplot(dataset, aes(x = type, y = meanExpression, fill = type ) )  +
+  geom_boxplot() +
+  coord_flip() + 
+  theme_minimal()
+plt1
+
+plt2 <- ggplot(dataset, aes(x = transcriptBiotype, y = meanExpression, fill = type ) )  +
+  geom_boxplot() + #outlier.shape = NA
+  coord_flip() + 
+  theme_minimal()
+plt2
