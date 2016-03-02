ac62be7bff22f15d105ade373f05c543b3e55aef
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-02 13:56:44 +0000

    minor changes

--- src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/expression_per_tissue.R
+++ src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/expression_per_tissue.R
@@ -6,7 +6,7 @@ library(gridExtra)
 
 #expression_input_file = "/home/diogo/testing/expression_test.csv"
 #expression_input_file = "/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/processing/expressionData/dataFiles/expression.csv"
-#expression_sample_input_file = "/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/processing/expressionData/dataFiles/expression_sample.csv"
+expression_input_file = "/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/processing/expressionData/dataFiles/expression_sample.csv"
 
 nc <- max(count.fields(expression_input_file, sep=","))
 
@@ -17,15 +17,17 @@ expression_df <- melt(expression_df)
 colourCount = length(unique(expression_df$variable))
 getPalette = colorRampPalette(brewer.pal(7, "Set1")) #this creates a function, which will take total number of shades of colours to use
 
-print("ALL SAMPLES: summary(expression_df$value[expression_df$variable == Brain]")
-print(summary(expression_df$value[expression_df$variable == "Brain"]) )
+print("ALL SAMPLES: summary(expression_df$value[expression_df$variable == Muscle − Skeletal]")
+print(summary(expression_df$value[expression_df$variable == "Muscle − Skeletal"]) )
 print("ALL SAMPLES: summary(expression_df$value[expression_df$variable == Fallopian Tube]")
 print(summary(expression_df$value[expression_df$variable == "Fallopian Tube"]) )
 
 plt1 <- ggplot(expression_df, aes(x = variable, y = log2(value) ) )  +
   geom_boxplot(fill=getPalette(colourCount)) +
   coord_flip() + 
+#  theme(axis.text=element_text(size=4)) +
   theme_minimal() +
+  theme(text = element_text(size=10)) +
   xlab("Tissues")
 #    scale_y_discrete(limits=c("0"))
 
@@ -37,6 +39,7 @@ plt2 <- ggplot(expression_df, aes(x = variable, y = log2(value) ) )  +
   geom_boxplot(fill=getPalette(colourCount)) +
   coord_flip() + 
   theme_minimal() +
+  theme(text = element_text(size=10)) +
   xlab("Tissues")
 #    scale_y_discrete(limits=c("0"))
 
