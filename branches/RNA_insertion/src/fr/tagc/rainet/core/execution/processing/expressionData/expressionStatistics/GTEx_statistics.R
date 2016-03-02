69c69ab52db44af79c68fbc9e69bf2e944a9758d
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-02 13:56:44 +0000

    minor changes

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/GTEx_statistics.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/GTEx_statistics.R
@@ -12,6 +12,20 @@ if( length(args) != 6){
   stop("Rscript: Bad argument number")
 }
 
+#From template: https://github.com/hadley/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
+grid_arrange_shared_legend <- function(...) {
+  plots <- list(...)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position="bottom"))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  grid.arrange(
+    do.call(arrangeGrob, lapply(plots, function(x)
+      x + theme(legend.position="none"))),
+    legend,
+    ncol = 1,
+    heights = unit.c(unit(1, "npc") - lheight, lheight))
+}
+
 working_dir = args[1]
 annotation_input_file = args[2]
 expression_input_file = args[3]
--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/transcript_expression_averages.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/expressionData/expressionStatistics/transcript_expression_averages.R
@@ -9,21 +9,8 @@ require(gridExtra)
 #tx_expression_avg_file = "/home/diogo/testing/tx_expression_avg.tsv"
 #tx_expression_avg_file = "/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/processing/expressionData/dataFiles/tx_expression_avg.tsv"
 
-#From template: https://github.com/hadley/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
-grid_arrange_shared_legend <- function(...) {
-  plots <- list(...)
-  g <- ggplotGrob(plots[[1]] + theme(legend.position="bottom"))$grobs
-  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
-  lheight <- sum(legend$height)
-  grid.arrange(
-    do.call(arrangeGrob, lapply(plots, function(x)
-      x + theme(legend.position="none"))),
-    legend,
-    ncol = 1,
-    heights = unit.c(unit(1, "npc") - lheight, lheight))
-}
-
 expression_df = read.table( tx_expression_avg_file, stringsAsFactors = FALSE, header = TRUE, sep="\t")
+
 
 print("summary(expression_df$ExprMean)")
 print(summary(expression_df$ExprMean))
@@ -32,10 +19,11 @@ print(summary(expression_df$ExprStd))
 print("summary(expression_df$ExprMedian)")
 print(summary(expression_df$ExprMedian))
 
-#NOTE THAT I USE XLIM! #I SHOULD SUM UP VALUES ABOVE XLIM
+#NOTE THAT I USE XLIM! #IDEALLY I SHOULD SUM UP VALUES ABOVE XLIM
 XLIM_MAX = 5
 print(paste("Plots with xlim:",XLIM_MAX, sep=" "))
 
+# Histogrames for all RPKMs of all transcripts
 plt1 <- ggplot(expression_df, aes(x = ExprMean ) )  +
   geom_histogram(binwidth = 0.1, fill="white", colour="black") + 
   geom_vline(aes(xintercept=mean(ExprMean)),color="blue", linetype="dashed", size=1) +
@@ -56,6 +44,7 @@ plt2 <- ggplot(expression_df, aes(x = ExprMedian ) )  +
   theme_minimal()
 plt2
 
+# Density plots for all RPKMs of each tissue
 plt3 <- ggplot(expression_df, aes(x = ExprMean ) )  +
   geom_line(stat="density",aes(color=TissueName)) +
   geom_vline(aes(xintercept=0.1),color="red", linetype="dashed", size=1) +
@@ -77,4 +66,14 @@ plt4
 
 grid.arrange( plt1, plt2)
 grid.arrange( plt3, plt4)
+
+
+# Scatterplot of Mean vs CoefVariation
+#plt6 <- ggplot(expression_df, aes(x=ExprMean, y=CoefVariation)) +
+#  geom_point(shape=1,alpha=1/4)
+#  geom_smooth(method=lm)
+#  xlim(c(0.0,300)) 
+#plt6
+
+
 
