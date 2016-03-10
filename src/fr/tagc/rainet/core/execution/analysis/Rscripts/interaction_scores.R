be4ecfdd2a25e6258e633e187a0bb5631ade179f
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-10 16:26:38 +0000

    no real changes

--- src/fr/tagc/rainet/core/execution/analysis/Rscripts/interaction_scores.R
+++ src/fr/tagc/rainet/core/execution/analysis/Rscripts/interaction_scores.R
@@ -7,16 +7,17 @@ require(gridExtra)
 
 ######### Interaction scores ##########
 
-#inputFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_results/real/Report/interaction_scores.tsv"
-#inputFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_expected/Report/interaction_scores.tsv"
-
 inputFile = paste( output_folder, report_interaction_scores, sep = "/")
+
+#testing
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/temp_results/files_for_barcelona_meeting/data_without_interaction_filter/interaction_scores.tsv"
+#inputFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_expected/Report/interaction_scores.tsv"
 
 print (paste("INPUT FILE:",inputFile) )
 
 nc <- max(count.fields(inputFile, sep=","))
 
-inputData <- read.table(inputFile, sep=",", row.names = 1, col.names=paste("V",1:nc,sep="."), fill=T)
+inputData <- read.csv(inputFile, sep=",", row.names = 1, col.names=paste("V",1:nc,sep="."), fill=T)
 
 interaction <- as.data.frame(t(inputData))
 interaction <- melt(interaction)
@@ -31,10 +32,8 @@ plt1 <- ggplot( interaction, aes(x = value)) +
 # Density plot of interaction scores per biotype
 plt2 <- ggplot( interaction, aes(x = value)) + 
   geom_density( aes( color = variable), size = 1) +
-#  geom_line( stat = "density", aes( color = variable) ) +
+  #  geom_line( stat = "density", aes( color = variable) ) +
   theme_minimal() +
   xlab( "Interaction scores")
 
-
 grid.arrange( plt1, plt2)
-
