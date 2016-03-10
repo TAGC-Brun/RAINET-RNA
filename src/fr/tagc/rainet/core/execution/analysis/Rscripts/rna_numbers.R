bb3b88b9f421cc7d5eb68ed151f42f7916c6a252
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-10 17:25:03 +0000

    added pie plots, but also some hard-coded values which may need to be modified

--- src/fr/tagc/rainet/core/execution/analysis/Rscripts/rna_numbers.R
+++ src/fr/tagc/rainet/core/execution/analysis/Rscripts/rna_numbers.R
@@ -28,3 +28,48 @@ plt1 <- ggplot( rnaNumbers, aes(x = variable, y = value, fill = Data)) +
   ylab( "Frequency")
 
 print(plt1)
+
+#### Pie plots ####
+
+# Examples from http://www.sthda.com/english/wiki/ggplot2-pie-chart-quick-start-guide-r-software-and-data-visualization
+
+# use only values after filtering
+afterFilt <- rnaNumbers[ rnaNumbers$Data == "After_RNA_filter",]
+
+# pie plot for broad categories of RNAs
+broadCat <- afterFilt[ afterFilt$variable == "MRNA" | afterFilt$variable == "OtherRNA" | afterFilt$variable == "LncRNA",]
+plt2 <- ggplot( broadCat, aes(x = "", y = value, fill = variable))  + 
+  geom_bar(width = 1, stat = "identity") + #aes(colour = Data) +
+  coord_polar(theta = "y") +
+  theme_minimal() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold"),
+    axis.text.x=element_blank()  ) +
+  geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), label = value), size=5)
+
+
+# pie plot for categories of lncRNAs
+lncRNABiotypes <- c("X3prime_overlapping_ncrna","antisense","bidirectional_promoter_lncrna","known_ncrna","lincRNA","macro_lncRNA","processed_transcript","sense_intronic","sense_overlapping","TEC")
+specCat <- afterFilt[ afterFilt$variable %in% lncRNABiotypes,]
+
+# Bar plot for numbers of items before and after filter
+plt3 <- ggplot( specCat, aes(x = "", y = value, fill = variable))  + 
+  geom_bar(width = 1, stat = "identity") + #aes(colour = Data) +
+  coord_polar(theta = "y") +
+  theme_minimal() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold"),
+    axis.text.x=element_blank()  ) +
+  geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), label = value), size=5)
+
+grid.arrange( plt2, plt3)
