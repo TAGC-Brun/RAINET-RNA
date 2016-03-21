7729bd897980ab715cd6745bd4e080834e48658d
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-21 15:28:28 +0000

    updated expression data report

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/Rscripts/analysis_strategy_report.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/Rscripts/analysis_strategy_report.R
@@ -9,7 +9,7 @@ library(knitr)
 args <- commandArgs(TRUE)
 
 # Test if we have enough arguments
-if( length(args) != 8){
+if( length(args) != 11){
   stop("Rscript: Bad argument number")
 }
 
@@ -18,8 +18,12 @@ sweave_file = args[2]
 output_folder = args[3]
 parameters_log = args[4]
 report_rna_numbers = args[5]
-report_interaction_numbers = args[6]
-report_interaction_scores = args[7]
-report_interaction_partners = args[8]
+report_rna_expression = args[6]
+report_rna_expression_data_presence = args[7]
+report_interactions_tissues_where_expressed = args[8]
+report_interaction_numbers = args[9]
+report_interaction_scores = args[10]
+report_interaction_partners = args[11]
+
 
 knit2pdf(sweave_file)
