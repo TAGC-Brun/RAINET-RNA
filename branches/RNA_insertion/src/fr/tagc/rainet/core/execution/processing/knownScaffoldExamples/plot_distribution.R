d0fe51de6dc34589f9bda087ef9c7474da2e63e6
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-25 09:08:18 +0000

    improved code for plotting distribution of Zscores

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/plot_distribution.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/plot_distribution.R
@@ -4,7 +4,14 @@ library(reshape)
 require(grid)
 require(gridExtra)
 
-inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/ENST00000565493_NORAD.tsv_zcores.tsv"
+args <- commandArgs(TRUE)
+# Test if we have enough arguments
+if( length(args) != 1){
+  stop("Rscript: Bad argument number")
+}
+inputFile = args[1]
+
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/ENST00000565493_NORAD.tsv_zcores.tsv"
 
 dataset = read.table( inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 
@@ -16,8 +23,7 @@ plt0 <- ggplot(dataset, aes(x=catrapid_zscore, colour = in_validated_set)) +
   theme_minimal() +
   xlab( "Z-score") +
   ylab( "Density") +
-  annotate("text",  x=Inf, y = Inf, label = paste("# True: ",nrow(dataset)-nrow(validated),"\n# False: ",nrow(validated)), vjust=1, hjust=1)
-plt0
+  annotate("text",  x=Inf, y = Inf, label = paste("# True: ",nrow(validated),"\n# False: ",nrow(dataset)-nrow(validated)), vjust=1, hjust=1)
 
 plt1 <- ggplot(dataset, aes(x=catrapid_zscore)) + 
   geom_histogram(binwidth=0.05, colour="black", fill="white") +
@@ -28,6 +34,11 @@ for (i in validated$catrapid_zscore){
   loop_input = paste("geom_point(aes(x=",i,",y=1, size=0.1, colour='red', position='dodge') )")
   plt1 <- plt1 + eval(parse(text=loop_input))  
   }
-plt1
+plt1 <- plt1 + guides(colour=FALSE)
+
+png(filename=paste(inputFile,".png",sep=""))
 
 grid.arrange(plt0, plt1)
+
+dev.off()
+
