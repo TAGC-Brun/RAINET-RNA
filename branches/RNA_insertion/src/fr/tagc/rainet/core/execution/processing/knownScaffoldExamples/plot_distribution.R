7a167d8af33d6569417b295338a3c918ee327b5c
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-03-29 12:07:56 +0000

    changed label of Z score to score

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/plot_distribution.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/plot_distribution.R
@@ -21,14 +21,14 @@ validated = dataset[dataset$in_validated_set == "True",]
 plt0 <- ggplot(dataset, aes(x=catrapid_zscore, colour = in_validated_set)) + 
   geom_density() + 
   theme_minimal() +
-  xlab( "Z-score") +
+  xlab( "Score") +
   ylab( "Density") +
   annotate("text",  x=Inf, y = Inf, label = paste("# True: ",nrow(validated),"\n# False: ",nrow(dataset)-nrow(validated)), vjust=1, hjust=1)
 
 plt1 <- ggplot(dataset, aes(x=catrapid_zscore)) + 
   geom_histogram(binwidth=0.05, colour="black", fill="white") +
   theme_minimal() +
-  xlab( "Z-score") +
+  xlab( "Score") +
   ylab( "Protein count")
 for (i in validated$catrapid_zscore){
   loop_input = paste("geom_point(aes(x=",i,",y=1, size=0.1, colour='red', position='dodge') )")
