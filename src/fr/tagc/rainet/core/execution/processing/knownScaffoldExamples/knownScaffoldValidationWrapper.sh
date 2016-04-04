0652c112c7748e0b7d5735e0d990623e136cdcb1
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-04-04 15:58:37 +0000

    now writing fragment score mean besides the max.

--- src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/knownScaffoldValidationWrapper.sh
+++ src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/knownScaffoldValidationWrapper.sh
@@ -17,18 +17,11 @@ topProportion='1'
 scoreColumn=1
 
 
-#### XIST MOUSE ####
-
-$script $catrapid_folder/all_interactions.85891.XIST_MOUSE_RBP_DBP_DISORDER.txt /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/XIST_paper_table_s1_tab2_uniprot.txt fakeFile $rainet_db $out_folder$scoreColumn --npinter 0 --columnForPlot $scoreColumn
-# $script $catrapid_folder/all_interactions.85891.XIST_MOUSE_RBP_DBP_DISORDER.txt /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/XIST_Tartaglia_paper_table_s1c.txt fakeFile $rainet_db $out_folder$scoreColumn --npinter 0 --columnForPlot $scoreColumn
-
 #### SAMMSON ####
 
 list_file="SAMMSON_keywords.tsv"
 
 $script $catrapid_folder/ENST00000483525_SAMMSON.tsv /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/SAMMSON_paper_table_s2_tab2_uniprot.txt fakeFile $rainet_db $out_folder$scoreColumn --npinter 0 --columnForPlot $scoreColumn --searchSpace /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/SAMMSON_paper_table_s2_all_ids.txt
-
-#exit
 
 #### NORAD ####
 
@@ -37,6 +30,11 @@ list_file="NORAD_keywords.tsv"
 $script $catrapid_folder/ENST00000565493_NORAD.tsv /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/NORAD_paper_table_s1_sense_only.txt fakeFile $rainet_db $out_folder$scoreColumn --npinter 0 --columnForPlot $scoreColumn --searchSpace /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/NORAD_paper_table_s1.txt
 
 #exit
+
+#### XIST MOUSE ####
+
+$script $catrapid_folder/all_interactions.85891.XIST_MOUSE_RBP_DBP_DISORDER.txt /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/XIST_paper_table_s1_tab2_uniprot.txt fakeFile $rainet_db $out_folder$scoreColumn --npinter 0 --columnForPlot $scoreColumn
+# $script $catrapid_folder/all_interactions.85891.XIST_MOUSE_RBP_DBP_DISORDER.txt /home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/knownScaffoldValidation/manualLists/XIST_Tartaglia_paper_table_s1c.txt fakeFile $rainet_db $out_folder$scoreColumn --npinter 0 --columnForPlot $scoreColumn
 
 #### NEAT1 ####
 
