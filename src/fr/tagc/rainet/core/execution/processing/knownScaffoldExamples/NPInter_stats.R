a444a9817128eb655ce6cb12f86ead398385e777
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-04-07 14:52:50 +0000

    major commenting and performance improvements. added FDR curves and stability analysis

--- src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/NPInter_stats.R
+++ src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/NPInter_stats.R
@@ -5,9 +5,8 @@ require(grid)
 require(gridExtra)
 library(data.table)
 library(Epi)
-library(pROC)
+#library(pROC)
 library(ROCR)
-
 
 args <- commandArgs(TRUE)
 # Test if we have enough arguments
@@ -16,15 +15,24 @@ if( length(args) != 1){
 }
 inputFile = args[1]
 
+
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/test_scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/scores_all_interactions.tsv"
 
 dataset <- fread(inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 
-# Get only the validated for drawing points on them
+# Separate validated from non-validated 
 validated = dataset[dataset$in_validated_set == 1,]
 nonValidated = dataset[dataset$in_validated_set == 0]
 
+###################################### 
+###################################### 
+### Plot catRAPID scores distributions in and outside NPInter 
+###################################### 
+###################################### 
+
+# testing difference of distributions (Kolmogorow-Smirnov  )
 print (ks.test(validated$catrapid_score, nonValidated$catrapid_score, alternative = c("two.sided")) )
 
 plt0 <- ggplot(dataset, aes(x=catrapid_score, colour = as.factor(in_validated_set))) + 
@@ -36,15 +44,19 @@ plt0 <- ggplot(dataset, aes(x=catrapid_score, colour = as.factor(in_validated_se
 plt0
 
 
+###################################### 
+###################################### 
+### ROC / cutoff analysis
+###################################### 
+###################################### 
 
-### ROC Curve ###
+###################################### 
+###################################### 
+## Random subsamples 
+###################################### 
+###################################### 
 
-calcThreshold <- function(response, predict) {
-  r <- pROC::roc(response, predict)
-  r$thresholds[which.max(r$sensitivities + r$specificities)]
-}
-
-# # Sub-sampling the non-validated dataset
+# Approach: Sub-sampling the non-validated dataset to match length of validated dataset, and keeping the validated dataset as it is
 
 repetitions = 100
 nsubsample = nrow(validated)
@@ -56,6 +68,7 @@ cutoffs = c()
 aurocs = c()
 kss = c()
 
+# Create ROC plots and other metrics
 for (i in seq(1, repetitions)){
   nonValidatedSample = nonValidated[sample( nrow(nonValidated), nsubsample ), ]
   datasetSample = rbind(nonValidatedSample, validated) 
@@ -64,18 +77,18 @@ for (i in seq(1, repetitions)){
   #   ks = ks.test(validated$catrapid_score, nonValidatedSample$catrapid_score, alternative = c("two.sided"))$p.value
   #   kss = c(kss, ks)
   
-  # Optimal cutoff / cutpoint
-  cutoff = calcThreshold(datasetSample$in_validated_set, datasetSample$catrapid_score)
-  cutoffs = c(cutoffs, cutoff)
-  
-  # ROC
+  # ROC using ROCR
   pred = prediction(datasetSample$catrapid_score, datasetSample$in_validated_set, label.ordering = NULL)
   perf = performance(pred, measure = "tpr", x.measure = "fpr")
   if (i == 1){
-    plot(perf, lty=5, col= colours[i],main="")
+    plot(perf, lty=5, col= colours[i],main="", ylab = "Sensitivity", xlab = "1-Specificity")
   }else{
     plot(perf, lty=5, col= colours[i],main="", add=TRUE)
   }
+
+  # Optimal cutoff / cutpoint
+  cutoff = perf@alpha.values[[1]][which.max(1-perf@x.values[[1]] + perf@y.values[[1]])] 
+  cutoffs = c(cutoffs, cutoff)
   
   # AUROC
   auc = performance(pred, measure = "auc")
@@ -83,50 +96,183 @@ for (i in seq(1, repetitions)){
   
 }
 
-# Means and stds of randomisations
-mean(cutoffs)
-sd(cutoffs)
+# Mean cutoff and std of randomisations
+paste("Mean cutoff:",mean(cutoffs),"std:",sd(cutoffs))
+# Mean auc and std of randomisations
+paste("Mean auroc:",mean(aurocs),"std:",sd(aurocs))
 
-mean(aurocs)
-sd(aurocs)
+###################################### 
+# FDR curves
+###################################### 
+# Note: I'm in fact resampling, not using the same samples in previous plot.
+# Problem is that I cannot store "plot" to a variable, and so, inside a loop I can only add data to one plot
+# I tried storing FDR prediction results in a vector and then loop them to create new plot but does not work
+# I cannot use performance objects to ggplots, I would have to create new data structure for ggplots
+# I could potentially store the sampling data and loop them
+for (i in seq(1, repetitions)){
+  nonValidatedSample = nonValidated[sample( nrow(nonValidated), nsubsample ), ]
+  datasetSample = rbind(nonValidatedSample, validated) 
+  
+  pred = prediction(datasetSample$catrapid_score, datasetSample$in_validated_set, label.ordering = NULL)
+  perf = performance(pred, measure = "pcfall")
+  if (i == 1){
+    plot(perf, lty=5, col= colours[i],main="",ylab = "False discovery rate FP/(TP+FP)", xlim = c(min(dataset$catrapid_score),max(dataset$catrapid_score)), ylim = c(0,1))
+  }else{
+    plot(perf, lty=5, col= colours[i],main="", add=TRUE)
+  }
+}
+abline(v = mean(cutoffs))
 
 
-# # Plotting ROC for the whole dataset (Time consuming)
+###################################### 
+###################################### 
+#### Whole dataset 
+###################################### 
+###################################### 
+
+pred = prediction(dataset$catrapid_score, dataset$in_validated_set, label.ordering = NULL)
+
+# ROC curve with ROCR package (fast)
+plot(performance(pred, measure = "tpr", x.measure = "fpr"))
+
+# Sensitivity plot
+plot(performance(pred, measure = "sens"))
+
+# Specificity plot
+plot(performance(pred, measure = "spec"))
+
+# False discovery rate on whole dataset
+#pcfall: Prediction-conditioned fallout. P(Y = - | Yhat = +). Estimated as: FP/(TP+FP).
+plot(performance(pred, measure = "pcfall"), ylab = "False discovery rate FP/(TP+FP)", ylim = c(0,1))
+
+###################################### 
+# Adding noise to dataset (missing information / stability analysis) 
+###################################### 
+# How does the ROC behave if I introduce some previously negatives as being positives
+
+# Separate validated from non-validated 
+validated = dataset[dataset$in_validated_set == 1,]
+nonValidated = dataset[dataset$in_validated_set == 0]
+
+repetitions = 10
+sampleSize = 1000 # how many negatives becoming positives
+
+cutoffs = c()
+aurocs = c()
+
+for (i in seq(1, repetitions)){
+  
+  # randomly pick indexes from negatives of where to make change
+  indexesToChange = sample( nrow(nonValidated), sampleSize )
+
+  # create new copy of nonValidated, which will be modified
+  copyNonValidated = nonValidated
+  
+  # now the non validated set will contain some validated items
+  copyNonValidated$in_validated_set[indexesToChange] = 1
+
+  # join 'non'-validated and validated
+  datasetSample = rbind(validated,copyNonValidated) 
+  
+  # ROC using ROCR
+  pred = prediction(datasetSample$catrapid_score, datasetSample$in_validated_set, label.ordering = NULL)
+  perf = performance(pred, measure = "tpr", x.measure = "fpr")
+  if (i == 1){
+    plot(perf, lty=5, col= colours[i],main="", ylab = "Sensitivity", xlab = "1-Specificity")
+  }else{
+    plot(perf, lty=5, col= colours[i],main="", add=TRUE)
+  }
+  
+  # Optimal cutoff / cutpoint
+  cutoff = perf@alpha.values[[1]][which.max(1-perf@x.values[[1]] + perf@y.values[[1]])] 
+  cutoffs = c(cutoffs, cutoff)
+  
+  # AUROC
+  auc = performance(pred, measure = "auc")
+  aurocs = c(aurocs, auc@y.values[[1]])
+  
+}
+
+# Mean cutoff and std of randomisations
+paste("Mean cutoff:",mean(cutoffs),"std:",sd(cutoffs))
+# Mean auc and std of randomisations
+paste("Mean auroc:",mean(aurocs),"std:",sd(aurocs))
+
+
+
+
+
+
+
+
+# png(filename=paste(inputFile,".png",sep=""))
+# dev.off()
+
+
+# ### Getting max sensitivity OR max specificity
+# 
+# r <- pROC::roc(datasetSample$in_validated_set, datasetSample$catrapid_score)
+# #r$thresholds[which.max(r$sensitivities + r$specificities)]
+# 
+# 
+# #Note: -inf will always give max sensitivities
+# # one option is to set a "cost" using ROCR, other is to look for local maximas
+# r$thresholds[which.max(r$sensitivities)]
+# r$thresholds[which.max(r$specificities)]
+# 
+# # # local maximas
+# 
+# r$thresholds
+# print (coords(roc=r, x = "local maximas", ret='threshold') )
+# 
+# # # cost option
+# pred = prediction(datasetSample$catrapid_score, datasetSample$in_validated_set, label.ordering = NULL)
+# 
+# # increasing cost of false negatives, i.e. get cutoff where no false negative is allowed
+# cost = performance(pred, "cost", cost.fp = 1, cost.fn = 10000000)
+# pred@cutoffs[[1]][which.min(cost@y.values[[1]])]
+# pred@fn[[1]][which.min(cost@y.values[[1]])]
+# pred@fp[[1]][which.min(cost@y.values[[1]])]
+# 
+# # increasing cost of false positives, i.e. get cutoff where no false positive is allowed
+# cost = performance(pred, "cost", cost.fp = 10000000, cost.fn = 1)
+# pred@cutoffs[[1]][which.min(cost@y.values[[1]])]
+# pred@fn[[1]][which.min(cost@y.values[[1]])]
+# pred@fp[[1]][which.min(cost@y.values[[1]])]
+
+
+# # Manually calculate FDR
+# cutoff = -50
+# 
+# above = datasetSample[datasetSample$catrapid_score >= cutoff]
+# below = datasetSample[datasetSample$catrapid_score < cutoff]
+# nrow(above)
+# nrow(below)
+# 
+# validated = datasetSample[datasetSample$in_validated_set == 1,]
+# nonValidated = datasetSample[datasetSample$in_validated_set == 0]
+# nrow(validated)
+# nrow(nonValidated)
+# 
+# aboveValidated = above[above$in_validated_set == 1,]
+# nrow(aboveValidated)
+# 
+# TP = nrow(aboveValidated) #nrow(validated)
+# FP = nrow(above) - nrow(aboveValidated)
+# 
+# TP
+# FP
+# 
+# FP / (FP + TP) 
+
+##
+# calcThreshold(datasetSample$in_validated_set, datasetSample$catrapid_score)
+# calcThreshold <- function(response, predict) {
+#   r <- pROC::roc(response, predict)
+#   r$thresholds[which.max(r$sensitivities + r$specificities)]
+# }
+
+# # Plotting ROC for the whole dataset with Epi package (Time consuming)
 # plt1 = ROC( form = in_validated_set ~ catrapid_score, data = dataset, plot="ROC", main="ROC curve", MI=TRUE, MX=TRUE, PV=TRUE) 
+# # Calculate cutoff maximizing sensitivity and specificity
 # print (calcThreshold(dataset$in_validated_set, dataset$catrapid_score))
-
-png(filename=paste(inputFile,".png",sep=""))
-dev.off()
-
-
-### Getting max sensitivity OR max specificity
-
-r <- pROC::roc(datasetSample$in_validated_set, datasetSample$catrapid_score)
-#r$thresholds[which.max(r$sensitivities + r$specificities)]
-
-#Note: -inf will always give max sensitivities
-# one option is to set a "cost" using ROCR, other is to look for local maximas
-r$thresholds[which.max(r$sensitivities)]
-r$thresholds[which.max(r$specificities)]
-
-# # local maximas
-
-r$thresholds
-print (coords(roc=r, x = "local maximas", ret='threshold') )
-
-# # cost option
-pred = prediction(datasetSample$catrapid_score, datasetSample$in_validated_set, label.ordering = NULL)
-
-# increasing cost of false negatives, i.e. get cutoff where no false negative is allowed
-cost = performance(pred, "cost", cost.fp = 1, cost.fn = 10000000)
-pred@cutoffs[[1]][which.min(cost@y.values[[1]])]
-pred@fn[[1]][which.min(cost@y.values[[1]])]
-pred@fp[[1]][which.min(cost@y.values[[1]])]
-
-# increasing cost of false positives, i.e. get cutoff where no false positive is allowed
-cost = performance(pred, "cost", cost.fp = 10000000, cost.fn = 1)
-pred@cutoffs[[1]][which.min(cost@y.values[[1]])]
-pred@fn[[1]][which.min(cost@y.values[[1]])]
-pred@fp[[1]][which.min(cost@y.values[[1]])]
-
-
