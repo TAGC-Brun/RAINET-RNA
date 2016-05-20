9dd41684f40578d6da614154275058738a8825b0
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-05-20 09:00:25 +0000

    commiting old change

--- branches/RNA_insertion/src/fr/tagc/rainet/core/util/option/OptionConstants.py
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/util/option/OptionConstants.py
@@ -24,39 +24,33 @@ HELP_TAG = "help"
 # The name of the various options
 #===============================================================================
 # Common options
-OPTION_DB_NAME = "databasePath"
-OPTION_SPECIES = "species"
-OPTION_VERBOSITY = "verbose"
+OPTION_DB_NAME = "Database path"
+OPTION_SPECIES = "Species"
+OPTION_VERBOSITY = "Verbosity"
 # Insertion strategy options
 OPTION_INSERTION_PROPERTIES_PATH = "insertionPropertiesPath"
 OPTION_INSERTION_FORCE_OVERRIDE = "insertionForceOverride"
 # InteractiveQuery Strategy options
 OPTION_QUERY_FILE = "query_file"
 # Analysis Strategy options
-OPTION_MINIMUM_INTERACTION_SCORE = "interaction_score"
-OPTION_TRANSCRIPT_BIOTYPE = "transcript_biotype"
-OPTION_LNCRNA_BIOTYPES = "lncRNA_biotypes"
-OPTION_GENCODE = "gencode_basic"
-OPTION_EXPRESSION_VALUE_CUTOFF = "expression_value_cutoff"
-OPTION_OUTPUT_FOLDER = "output_folder"
+OPTION_MINIMUM_INTERACTION_SCORE = "Minimum interaction score"
+OPTION_RNA_BIOTYPES = "RNA biotypes filtering"
+OPTION_GENCODE = "Gencode basic filtering"
+OPTION_EXPRESSION_VALUE_CUTOFF = "Minimum expression value"
+OPTION_OUTPUT_FOLDER = "Output folder"
 
 #===============================================================================
 # Constants for analysis strategy
 #===============================================================================
 
 DEFAULT_BIOTYPE = "RNA"
-DEFAULT_LNCRNA_BIOTYPES = "OFF"
 DEFAULT_INTERACTION_SCORE = "OFF"
 DEFAULT_GENCODE = 0
 DEFAULT_EXPRESSION_VALUE_CUTOFF = 0
 DEFAULT_OUTPUT_FOLDER = os.getcwd()
 
-BIOTYPE_LNCRNA = "LncRNA"
-BIOTYPE_MRNA = "MRNA"
-BIOTYPE_OTHERRNA = "OtherRNA"
-TRANSCRIPT_BIOTYPES = [ DEFAULT_BIOTYPE, BIOTYPE_MRNA, BIOTYPE_OTHERRNA, BIOTYPE_LNCRNA ]
-LNCRNA_BIOTYPES = DataConstants.RNA_LNCRNA_BIOTYPE
-
+RNA_BIOTYPES = DataConstants.RNA_MRNA_BIOTYPE[:] + DataConstants.RNA_LNCRNA_BIOTYPE[:]
+DEFAULT_RNA_BIOTYPES = RNA_BIOTYPES[:]
 
 #===============================================================================
 # The definition of the options
@@ -79,8 +73,7 @@ OPTION_LIST = {  "Insertion": [
                     [ "-v", "--verbose", "store", "string", OPTION_VERBOSITY, Constants.MODE_INFO, "The level of verbosity. Must be one of : " + str( Constants.VERBOSITY_LEVELS)],
                     [ "-o", "--outputFolder", "store", "string", OPTION_OUTPUT_FOLDER, DEFAULT_OUTPUT_FOLDER, "Folder where output files will be written."],
                     [ "-m", "--minimumInteractionScore", "store", "string", OPTION_MINIMUM_INTERACTION_SCORE, DEFAULT_INTERACTION_SCORE, "Protein-RNA interactions with interaction score below given value will be excluded from analysis. Default: OFF"],
-                    [ "-b", "--transcriptBiotype", "store", "string", OPTION_TRANSCRIPT_BIOTYPE, DEFAULT_BIOTYPE, "General transcript biotype to be INCLUDED in analysis. Default = RNA (i.e. any type of RNA). Can be one of :"+ str(TRANSCRIPT_BIOTYPES)],
-                    [ "-l", "--lncRNABiotype", "store", "string", OPTION_LNCRNA_BIOTYPES, DEFAULT_LNCRNA_BIOTYPES, "Comma-separated list of lncRNA subtypes to be INCLUDED in analysis. Default: OFF (i.e. all subtypes are considered). Can one or several of :"+ str(LNCRNA_BIOTYPES)],
+                    [ "-b", "--RNABiotype", "store", "string", OPTION_RNA_BIOTYPES, DEFAULT_RNA_BIOTYPES, "Comma-separated list of RNA biotypes to be INCLUDED in analysis. Default: all biotypes are considered. Can one or several of :"+ str(RNA_BIOTYPES)],
                     [ "-g", "--gencodeBasicOnly", "store", "int", OPTION_GENCODE, DEFAULT_GENCODE, "If 1, include in analysis ONLY transcripts tagged as present in 'GENCODE basic'. Default: 0 (i.e. all RNAs are considered)."],
                     [ "-e", "--expressionValueCutoff", "store", "float", OPTION_EXPRESSION_VALUE_CUTOFF, DEFAULT_EXPRESSION_VALUE_CUTOFF, "Protein-RNA interactions where one of its components has expression below the given value will be excluded. Default: OFF"]
                 ],
