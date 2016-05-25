0bab2e0213e82db1d6efb2b6e96bb1b10322b5bd
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-05-25 14:12:27 +0000

    minor improvements

--- src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/score_per_annotation.R
+++ src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/score_per_annotation.R
@@ -6,11 +6,18 @@ require(gridExtra)
 library(data.table)
 library(plyr)
 
-inputFile1 = "/home/diogo/testing/RBPDomainScore/output/annotated_interactions.tsv"
+inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/output/annotated_interactions.tsv"
 inputFile2 = "/home/diogo/testing/RBPDomainScore/cdna/output/annotated_interactions.tsv"
+
+inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/output_domains/annotated_interactions.tsv"
+inputFile2 = "/home/diogo/testing/RBPDomainScore/cdna/output_domains/annotated_interactions.tsv"
 
 dataset1 <- fread(inputFile1, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 dataset2 <- fread(inputFile2, stringsAsFactors = FALSE, header = TRUE, sep="\t")
+
+#adding type before merging datasets
+dataset1$type = "lncRNA"
+dataset2$type = "mRNA"
 
 head(dataset1)
 head(dataset2)
@@ -21,31 +28,42 @@ dataset2count = count(dataset2, 'annotation')
 table = merge( dataset1count, dataset2count, by="annotation", all = TRUE, suffixes = c(".lncRNA", ".mRNA"))
 table
 
-plt1 <- ggplot(dataset1, aes(x = annotation, y = score) )  +
-  geom_boxplot(outlier.shape = NA, fill = "red") +
-  coord_flip() + 
+# merge/append the two datasets
+mergedDataset = rbind( dataset1, dataset2)
+
+#cat(dataset1$uniprotac[dataset1$annotation == "Unclassified"], sep = "\n")
+#cat(dataset2$uniprotac[dataset2$annotation == "Unclassified"], sep = "\n")
+
+####################
+## Plot distributions
+####################
+
+plt1 <- ggplot(dataset1 )  +
+  geom_density(data = dataset1, aes(x = score, colour = annotation) ) +
   theme_minimal()
 plt1
 
-plt2 <- ggplot(dataset2, aes(x = annotation, y = score ) )  +
-  geom_boxplot(outlier.shape = NA, fill = "blue") +
-  coord_flip() + 
+plt2 <- ggplot(dataset2 )  +
+  geom_density(data = dataset2, aes(x = score, colour = annotation) ) +
   theme_minimal()
 plt2
 
 grid.arrange(plt1,plt2)
 
-plt3 <- ggplot()  +
-  geom_boxplot(outlier.shape = NA, data = dataset1, aes(x = annotation, y = score, colour = "lncRNA" ) ) +
-  geom_boxplot(outlier.shape = NA, data = dataset2, aes(x = annotation, y = score, colour = "mRNA" ) ) +
-  scale_color_manual(name = "Dataset", values = c("red","blue")) +
+plt3 <- ggplot(data = mergedDataset, aes(x = annotation, y = score, fill = type))  +
+  geom_boxplot(outlier.shape = NA, position = "dodge" ) +
   coord_flip() + 
   theme_minimal()
 plt3
 
+# plt3 <- ggplot()  +
+#   geom_boxplot(outlier.shape = NA, data = dataset1, aes(x = annotation, y = score, colour = "lncRNA" ) ) +
+#   geom_boxplot(outlier.shape = NA, data = dataset2, aes(x = annotation, y = score, colour = "mRNA" ) ) +
+#   scale_color_manual(name = "Dataset", values = c("red","blue")) +
+#   coord_flip() + 
+#   theme_minimal()
+# plt3
 
-#> 105+40+123+615+147+56
-#[1] 1086
 
 ####################
 ## Statistical tests
@@ -70,12 +88,15 @@ lncRNAClassical_NonRBP
 #test lncRNA classical vs non-classical
 mRNAClassical = dataset2$score[ dataset2$annotation == "Classical"]
 mRNANonClassical = dataset2$score[ dataset2$annotation == "Non-classical"]
+mRNANonRBP = dataset2$score[ dataset2$annotation == "Non-RBP"]
+
 summary( mRNAClassical)
 summary( mRNANonClassical)
 summary( mRNANonRBP)
 
 mRNAClassical_NonClassical = ks.test(mRNAClassical, mRNANonClassical, alternative = c("two.sided"))
 mRNAClassical_NonClassical
-# for mRNA non classical are significantly higher than classical!
+mRNAClassical_NonRBP = ks.test(mRNAClassical, mRNANonRBP, alternative = c("two.sided"))
+mRNAClassical_NonRBP
 
 
