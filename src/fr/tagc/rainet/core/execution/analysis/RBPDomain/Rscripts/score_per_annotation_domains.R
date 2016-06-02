5bb32f68f70cd9861a0153dd276b267b31e4da57
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-06-02 16:11:12 +0000

    updated / used for TF addition and target RNA analysis

--- src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/score_per_annotation_domains.R
+++ src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/score_per_annotation_domains.R
@@ -9,9 +9,14 @@ library(plyr)
 # function for number of observations 
 give.n <- function(x){   return(c(y = -10, label = length(x))) }
 
-# domain analysis files
-inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/domains/annotated_interactions.tsv"
-inputFile2 = "/home/diogo/testing/RBPDomainScore/mrna/domains/annotated_interactions.tsv"
+# # domain analysis files
+# inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/domains/annotated_interactions.tsv"
+# inputFile2 = "/home/diogo/testing/RBPDomainScore/mrna/domains/annotated_interactions.tsv"
+
+# domain analysis files + cutoff + TF
+inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/domains_TF/annotated_interactions.tsv"
+inputFile2 = "/home/diogo/testing/RBPDomainScore/mrna/domains_TF/annotated_interactions.tsv"
+
 
 dataset1 <- fread(inputFile1, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 dataset2 <- fread(inputFile2, stringsAsFactors = FALSE, header = TRUE, sep="\t")
@@ -19,6 +24,9 @@ dataset2 <- fread(inputFile2, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 #adding type before merging datasets
 dataset1$type = "lncRNA"
 dataset2$type = "mRNA"
+
+head(dataset1)
+head(dataset2)
 
 # dataset1Count = count(dataset1, 'annotation')
 # dataset2Count = count(dataset2, 'annotation')
@@ -42,7 +50,7 @@ dataset2count = count(dataset2, 'annotation')
 ## Identify top domains manually
 dataset1count[order(dataset1count$freq),]
 # list of domains we want to plot (top 15 domains)
-wantedDomains = c("RRM_1,RRM_5", "Linker_histone", "Pro_isomerase", "RRM_1,RBM1CTR", "zf-CCCH", "APOBEC_N", "KH_1", "MH2,MH1", "Pkinase", "Helicase_C,DEAD", "RnaseA", "LSM", "WD40", "RRM_1", "Non-RBP")
+wantedDomains = c("RRM_1,RRM_5", "Linker_histone", "Pro_isomerase", "RRM_1,RBM1CTR", "zf-CCCH", "APOBEC_N", "KH_1", "MH2,MH1", "Pkinase", "Helicase_C,DEAD", "RnaseA", "LSM", "WD40", "RRM_1", "Non-binding","TF")
 
 ### Filter dataset for having only those top domains
 dataset1TopDomains = dataset1[ dataset1$annotation %in% wantedDomains]
@@ -61,20 +69,6 @@ table
 ## Plot distributions
 ####################
 
-plt1 <- ggplot(dataset1TopDomains )  +
-  geom_density(data = dataset1TopDomains, aes(x = mean_score, colour = annotation) ) +
-  ggtitle(dataset1TopDomains$type) +
-  theme_minimal()
-
-plt2 <- ggplot(dataset2TopDomains )  +
-  geom_density(data = dataset2TopDomains, aes(x = mean_score, colour = annotation)) +
-  theme_minimal() +
-  theme(legend.position="none") +
-  ggtitle(dataset2TopDomains$type)
-plt2
-
-grid.arrange(plt1,plt2)
-
 plt3 <- ggplot(data = mergedDataset, aes(x = annotation, y = mean_score, fill = type))  +
   geom_boxplot(outlier.shape = NA, position = "dodge" ) +
   stat_summary(fun.data = give.n, geom = "text", size = 4) +
@@ -89,7 +83,7 @@ plt3
 #test lncRNA domains
 lncRNARRM_1 = dataset1TopDomains$mean_score[ dataset1TopDomains$annotation == "RRM_1"]
 lncRNALSM = dataset1TopDomains$mean_score[ dataset1TopDomains$annotation == "LSM"]
-lncRNANonRBP = dataset1TopDomains$mean_score[ dataset1TopDomains$annotation == "Non-RBP"]
+lncRNANonRBP = dataset1TopDomains$mean_score[ dataset1TopDomains$annotation == "Non-binding"]
 summary( lncRNARRM_1)
 summary( lncRNALSM)
 summary( lncRNANonRBP)
@@ -100,7 +94,7 @@ ks.test(lncRNALSM, lncRNANonRBP, alternative = c("two.sided"))
 #test mRNA domains
 mRNARRM_1 = dataset2TopDomains$mean_score[ dataset2TopDomains$annotation == "RRM_1"]
 mRNALSM = dataset2TopDomains$mean_score[ dataset2TopDomains$annotation == "LSM"]
-mRNANonRBP = dataset2TopDomains$mean_score[ dataset2TopDomains$annotation == "Non-RBP"]
+mRNANonRBP = dataset2TopDomains$mean_score[ dataset2TopDomains$annotation == "Non-binding"]
 summary( mRNARRM_1)
 summary( mRNALSM)
 summary( mRNANonRBP)
@@ -117,8 +111,9 @@ ks.test(lncRNAMH2MH1, mRNAMH2MH1, alternative = c("two.sided"))
 ## Number of RNAs above (count)
 ####################
 
-inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/domains_cutoff15/annotated_interactions.tsv"
-inputFile2 = "/home/diogo/testing/RBPDomainScore/mrna/domains_cutoff50/annotated_interactions.tsv"
+# domain analysis files + cutoff + TF
+inputFile1 = "/home/diogo/testing/RBPDomainScore/lncrna/domains_TF_cutoff15/annotated_interactions.tsv"
+inputFile2 = "/home/diogo/testing/RBPDomainScore/mrna/domains_TF_cutoff50/annotated_interactions.tsv"
 
 dataset1 <- fread(inputFile1, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 dataset2 <- fread(inputFile2, stringsAsFactors = FALSE, header = TRUE, sep="\t")
@@ -131,20 +126,20 @@ dataset2TopDomains$type = "mRNA"
 plt3 <- ggplot(data = dataset1TopDomains, aes(x = annotation, y = count))  +
   geom_boxplot(outlier.shape = NA, position = "dodge", fill = "red" ) +
   stat_summary(fun.data = give.n, geom = "text", size = 4) +
-  ggtitle(dataset1$type) +
+  ggtitle(dataset1TopDomains$type) +
   coord_flip() + 
   theme_minimal()
 
 plt4 <- ggplot(data = dataset2TopDomains, aes(x = annotation, y = count))  +
   geom_boxplot(outlier.shape = NA, position = "dodge", fill = "blue" ) +
   stat_summary(fun.data = give.n, geom = "text", size = 4) +
-  ggtitle(dataset2$type) +
-  coord_flip() + 
+  coord_flip() +
+  ggtitle(dataset2TopDomains$type) +
   theme_minimal()
 
 grid.arrange(plt3,plt4)
 
 mRNALSM_count = dataset2TopDomains$count[ dataset2TopDomains$annotation == "LSM"]
-mRNANonRBP_count = dataset2TopDomains$count[ dataset2TopDomains$annotation == "Non-RBP"]
+mRNANonRBP_count = dataset2TopDomains$count[ dataset2TopDomains$annotation == "Non-binding"]
 ks.test(mRNALSM_count, mRNANonRBP_count, alternative = c("two.sided"))
 
