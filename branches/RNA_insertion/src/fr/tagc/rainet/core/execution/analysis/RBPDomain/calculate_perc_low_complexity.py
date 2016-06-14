5a51acc21ea31911d4b75f0486b0e72d89ae925a
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-06-14 17:10:37 +0000

    changes to output file

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/RBPDomain/calculate_perc_low_complexity.py
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/RBPDomain/calculate_perc_low_complexity.py
@@ -40,7 +40,7 @@ for line in inFile:
 
 fastaData[ID] = text #for the last iteration
 
-outFile.write( "uniprotac\ttotal_length\tlowercase\tuppercase\tperc_low\n")
+outFile.write( "uniprotac\ttotal_length\tlowercase\tuppercase\ttag\tperc_low\n")
 
 # for each entry, calculate percentage underscore, write to file
 for ID in fastaData:
@@ -48,11 +48,45 @@ for ID in fastaData:
     lowerN = n_lower_chars( seq)
     upperN = n_upper_chars( seq)
     perc = lowerN * 100.0 / (lowerN + upperN) 
+    
+    # measure presence of low complexity in the terminus region (first 25 and last 25 aa)
+
+    # From Coletta et al. 2010
+    # t-LCRs were defined as regions starting or ending at no more than 25 amino acids from either sequence
+    # extremity, c-LCRs as regions starting or ending at least 50 amino acids from either sequence extremity.
+    
+    terminus = seq[0:25] + seq[-25:]
+    central = seq[50:-50]
+
+    assert len(terminus) == 50
+    assert len(central) <= max(len(seq) - 100, 0)
+
+    # location tags:
+    # 0 - no LC
+    # 1 - terminus LC
+    # 2 - central LC
+    # 3 - other LC
+
+    terminusBool = n_lower_chars( terminus) > 0
+    centralBool = n_lower_chars( central) > 0
+    otherBool = n_lower_chars( seq) > 0
+
+    tag = -1
+    if terminusBool and centralBool:
+        tag = "other LC"
+    elif terminusBool == 0 and centralBool == 0 and otherBool:
+        tag = "other LC"
+    elif terminusBool and centralBool == 0:
+        tag = "terminus LC"
+    elif terminusBool == 0 and centralBool:
+        tag = "central LC"
+    else:
+        tag = "no LC"
 
     assert lowerN + upperN == len(seq), "number of lower and upper characters should sum up to protein length, if not there may be wrong characters."
     assert perc >= 0 and perc <= 100
     
-    outFile.write ("%s\t%i\t%i\t%i\t%.2f\n" % (ID, len(seq), lowerN, upperN, perc) )
+    outFile.write ("%s\t%i\t%i\t%i\t%s\t%.2f\n" % (ID, len(seq), lowerN, upperN, tag, perc) )
 
 outFile.close()
 
