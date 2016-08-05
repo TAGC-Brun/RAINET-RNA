f6c20bd8fc07b383b63c46cb975f755786ac9472
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-08-05 17:08:39 +0000

    small corrections

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/EnrichmentAnalysisStategy.py
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/EnrichmentAnalysisStategy.py
@@ -55,6 +55,9 @@ from fr.tagc.rainet.core.data.InteractingProtein import InteractingProtein
 from fr.tagc.rainet.core.data.InteractingRNA import InteractingRNA
 from fr.tagc.rainet.core.data.BioplexCluster import BioplexCluster
 from fr.tagc.rainet.core.data.ProteinBioplexAnnotation import ProteinBioplexAnnotation
+from fr.tagc.rainet.core.data.ProteinWanAnnotation import ProteinWanAnnotation
+from fr.tagc.rainet.core.data.ProteinCorumAnnotation import ProteinCorumAnnotation
+
 
 from fr.tagc.rainet.core.data import DataConstants
 from fr.tagc.rainet.core.util import Constants
