2d846103dda32563d1d6731f1bc578dc344c1819
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-08-10 09:56:04 +0000

    minor improvements to plot

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/Rscripts/interaction_partners.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/Rscripts/interaction_partners.R
@@ -7,7 +7,9 @@ require(gridExtra)
 
 ######## Interaction Partners #########
 
-#inputFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_results/real/Report/interaction_partners.tsv"
+#inputFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_results/real/Report/interaction_partners_biotype.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/analysisStrategy/real_linc_expr/expr1.0/Report/interaction_partners_biotype.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/analysisStrategy/real_lncrna_expr/stats/Report/interaction_partners_biotype.tsv"
 
 inputFile = paste( output_folder, report_interaction_partners, sep = "/")
 
@@ -20,13 +22,17 @@ inputData <- read.table(inputFile, sep=",", row.names = 1, col.names=paste("V",1
 interaction <- as.data.frame(t(inputData))
 interaction <- melt(interaction)
 
+#summary(interaction$value[interaction$variable == "lincRNA"])
 
 # Density plot of interaction partners per biotype
 plt1 <- ggplot( interaction, aes(x = value)) + 
-  geom_histogram(binwidth = 1, fill="white", colour="black") + 
+  geom_histogram(binwidth = 25, fill="white", colour="black") + 
   theme_minimal() +
   xlab( "Interaction partners") +
   ylab( "Transcript count")
+plt1
+
+mean(interaction$value, na.rm = T)
 
 # Density plot of interaction partners per biotype
 plt2 <- ggplot( interaction, aes(x = value)) + 
@@ -35,3 +41,5 @@ plt2 <- ggplot( interaction, aes(x = value)) +
   xlab( "Interaction partners")
 
 grid.arrange( plt1, plt2)
+
+
