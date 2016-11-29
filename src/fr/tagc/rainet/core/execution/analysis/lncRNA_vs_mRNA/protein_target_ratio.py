61825a701a7ce515495907cfe919605fcabb9666
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2016-11-29 16:29:09 +0000

    working version of script to calculate protein target ratio

---
+++ src/fr/tagc/rainet/core/execution/analysis/lncRNA_vs_mRNA/protein_target_ratio.py
@@ -0,0 +1,182 @@
+import sys
+import os
+import argparse
+
+# import numpy as np
+# import pandas as pd
+
+from fr.tagc.rainet.core.util.exception.RainetException import RainetException
+from fr.tagc.rainet.core.util.log.Logger import Logger
+from fr.tagc.rainet.core.util.time.Timer import Timer
+
+# from fr.tagc.rainet.core.util.subprocess.SubprocessUtil import SubprocessUtil
+# from fr.tagc.rainet.core.util.sql.SQLManager import SQLManager
+# from fr.tagc.rainet.core.util.data.DataManager import DataManager
+
+# from fr.tagc.rainet.core.data.Protein import Protein
+
+#===============================================================================
+# Started 29-Nov-2016 
+# Diogo Ribeiro
+DESC_COMMENT = "Script to analyse for each protein, the amount and ratio between lncRNA and mRNA targets"
+SCRIPT_NAME = "protein_target_ratio.py"
+#===============================================================================
+
+#===============================================================================
+# General plan:
+# 1)
+# 2)
+#===============================================================================
+
+#===============================================================================
+# Processing notes:
+# 1)
+# 2)
+#===============================================================================
+
+# #
+def read_transcript_types( transcriptTypesFile):
+
+    # Example format
+    # "ENST00000000412","protein_coding","MRNA"
+    # "ENST00000000442","protein_coding","MRNA"
+
+    transcriptType = {} # key -> transcriptID, value -> type
+
+    with open( transcriptTypesFile, "r") as inFile:
+        for line in inFile:
+            spl = line.strip().split(",")
+            transcriptID = spl[0].replace('"','')
+            type = spl[2].replace('"','')
+            
+            transcriptType[ transcriptID] = type
+    
+    print "read_transcript_types: Collected types of %s transcripts" % len( transcriptType)
+
+    return transcriptType
+
+# #
+def read_interaction_file( interactionFile, transcriptType):
+   
+    # Example format 
+    # sp|O00425|sp ENST00000217233    1
+
+    typeStats = {} # key -> proteinID, value -> dict. Key -> target type, value -> count
+
+    proteinSet = set()
+    transcriptSet = set()
+    transcriptSetType = {} # key -> type, value -> set of transcripts
+
+    # stores targets for which we do not know the biotype
+    missingTargets = set()
+
+    countLines = 0
+
+    with open( interactionFile, "r") as inFile:
+        for line in inFile:
+            spl = line.split(" ")
+
+            countLines+= 1 
+
+            if countLines % 10000000 == 0:
+                print "Processed %s interactions" % countLines
+                           
+            proteinID = spl[0].split( "|")[1]
+            spl2 = spl[1].split( "\t")
+            transcriptID = spl2[0]
+            intScore = float( spl2[1])
+            
+            # get transcript type
+            if transcriptID in transcriptType:
+                txType = transcriptType[ transcriptID]
+            else:
+                # if not found, skip
+                missingTargets.add( transcriptID)
+                continue
+    
+            if proteinID not in typeStats:
+                typeStats[ proteinID] = {}
+            
+            if txType not in typeStats[ proteinID]:
+                typeStats[ proteinID][ txType] = 0
+                
+            if txType not in transcriptSetType:
+                transcriptSetType[ txType] = set()
+                
+            transcriptSetType[ txType].add( transcriptID)
+
+            # increment according to target type
+            typeStats[ proteinID][ txType] += 1
+
+            proteinSet.add(proteinID)
+            transcriptSet.add( transcriptID)
+
+    print "read_interaction_file: %s unique proteins" % len(proteinSet)
+    print "read_interaction_file: %s unique transcripts" % len(transcriptSet)
+    print "read_interaction_file: %s target with biotype not found" % len(missingTargets)
+
+    ### report on transcript types
+    
+    for type in transcriptSetType:
+        print type, len( transcriptSetType[ type])
+    
+    ### write output file
+
+    outFile = open("protein_target_ration.tsv","w")
+
+    outFile.write("proteinID\tn_mRNA\tn_lncRNA\tratio_mRNA_lncRNA\n")
+
+    for protID in typeStats:
+        
+        nMRNA = typeStats[ protID]["MRNA"]
+        nLNCRNA = typeStats[ protID]["LncRNA"]
+
+        if nLNCRNA > 0:
+            ratio = float(nMRNA) / nLNCRNA
+        else:
+            ratio = "NA"
+        
+        outFile.write( "%s\t%s\t%s\t%.2f\n" % ( protID, nMRNA, nLNCRNA, ratio) )
+
+    outFile.close()
+
+
+if __name__ == "__main__":
+
+    try:
+    
+        # Start chrono
+        Timer.get_instance().start_chrono()
+        print "STARTING " + SCRIPT_NAME
+        
+        #===============================================================================
+        # Get input arguments
+        #===============================================================================
+        parser = argparse.ArgumentParser(description= DESC_COMMENT) 
+    
+        # positional args
+        parser.add_argument('interactionFile', metavar='interactionFile', type=str,
+                             help='Catrapid interactions file.')
+        parser.add_argument('transcriptTypesFile', metavar='transcriptTypesFile', type=str,
+                             help='RAINET DB RNA table dump. E.g. "ENST00000001146","protein_coding","MRNA"')
+           
+        #gets the arguments
+        args = parser.parse_args( ) 
+    
+        #===============================================================================
+        # Run analysis / processing
+        #===============================================================================
+
+        Timer.get_instance().step( "Read transcript types file..")            
+        transcriptType = read_transcript_types( args.transcriptTypesFile)
+
+        Timer.get_instance().step( "Read interaction file..")            
+        read_interaction_file( args.interactionFile, transcriptType)
+
+        # Stop the chrono      
+        Timer.get_instance().stop_chrono( "FINISHED " + SCRIPT_NAME )
+
+    # Use RainetException to catch errors
+    except RainetException as rainet:
+        Logger.get_instance().error( "Error during execution of %s. Aborting :\n" % SCRIPT_NAME + rainet.to_string())
+
