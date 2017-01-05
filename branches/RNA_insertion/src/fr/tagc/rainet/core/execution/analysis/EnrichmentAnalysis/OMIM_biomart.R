cb2a4e96225f4a9dd169a1489310d35fd192e334
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-01-05 14:04:47 +0000

    update to match script-runnable version

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/OMIM_biomart.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/OMIM_biomart.R
@@ -10,27 +10,33 @@
 
 library("biomaRt")
 
+# Get input arguments
+#########################################################################
+
+# Get the arguments from the launch command line
+args <- commandArgs(TRUE)
+
+# Test if we have enough arguments
+if( length(args) != 1){
+  stop("Rscript: Bad argument number")
+}
+
+outputFile = args[1]  #"/home/diogo/Documents/RAINET_data/OMIM/OMIM_biomart.txt"
+
 # Setting up constant variables
 ##########################################################################
 
-#Note: as of 19-Jan-2016, it is not possible to access BioMart through the www.biomart.org/ , only access is through www.ensembl.org
 ENSEMBL_URL = "ensembl.org" #this is link to current Ensembl version 87
 ENSEMBL_BIOMART = "ENSEMBL_MART_ENSEMBL"
 BIOMART_DATASET = "hsapiens_gene_ensembl"
 
-BIOMART_CENTRAL_ATTRIBUTE = "ensembl_transcript_id"
-
 WANTED_ATTRIBUTES =  c(
-#  "ensembl_peptide_id",
+  "ensembl_gene_id",
   "mim_morbid",
-#    "hgnc_symbol",
+  "hgnc_symbol",
   "uniprot_swissprot"
 #  "mim_gene_accession",
   )
-
-OUTPUT_ATTRIBUTES = "/home/diogo/Documents/RAINET_data/OMIM/OMIM_biomart.txt"
-
-BATCH_SIZE = 500 #500 is the advised maximum limit of filter items from BioMart
 
 # Establishing server connection
 ##########################################################################
@@ -47,8 +53,8 @@ paste("Total number of entries:",nrow(allAtt))
 # write to output
 ##########################################################################
 header = capture.output(cat(WANTED_ATTRIBUTES,sep ="\t") )
-write.table(header,file = OUTPUT_ATTRIBUTES,quote = FALSE,sep="\t", append = TRUE,  row.names = FALSE, col.names = FALSE)
-write.table(allAtt,file = OUTPUT_ATTRIBUTES, quote = FALSE, sep="\t", row.names = FALSE, col.names = FALSE, append = TRUE)
+write.table(header,file = outputFile,quote = FALSE,sep="\t", append = TRUE,  row.names = FALSE, col.names = FALSE)
+write.table(allAtt,file = outputFile, quote = FALSE, sep="\t", row.names = FALSE, col.names = FALSE, append = TRUE)
 
 print ("FINISHED!")
 
