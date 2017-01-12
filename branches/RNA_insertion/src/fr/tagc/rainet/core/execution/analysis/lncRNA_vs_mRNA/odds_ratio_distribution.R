998da3a4cde80141480ca204dfea8b485f96225a
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-01-12 15:10:12 +0000

    R script to plot odds_ratio / t_test statistic distribution

---
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/lncRNA_vs_mRNA/odds_ratio_distribution.R
@@ -0,0 +1,27 @@
+
+library(data.table)
+require(ggplot2)
+require(grid)
+require(gridExtra)
+library(RColorBrewer)
+source("/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/r_functions.R")
+
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/mrna_vs_lncrna/protein_target_ratio_cutoff50_cutoff50_forR.tsv"
+
+inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/mrna_vs_lncrna/t_test/protein_target_ratio_ttest.out"
+
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/mrna_vs_lncrna/RBP_only/protein_target_ratio_rbp_only_cutoff50_cutoff50.tsv_forR"
+
+dataset <- fread(inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
+
+min(dataset$t_test_statistic)
+max(dataset$t_test_statistic)
+
+
+plt1 <- ggplot( dataset, aes(x = dataset$t_test_statistic)) + 
+  geom_histogram() + 
+  xlab("t test statistic") +
+  ylab("# proteins (whole proteome)") +
+  theme_minimal()
+plt1
+
