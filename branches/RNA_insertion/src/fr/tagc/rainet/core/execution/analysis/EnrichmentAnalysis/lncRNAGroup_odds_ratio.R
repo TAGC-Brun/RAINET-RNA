6b4c51cce9aba6b125d4129997f8ba2e1e9e3fa2
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-02-20 16:35:19 +0000

    outputting summary table

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/lncRNAGroup_odds_ratio.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/lncRNAGroup_odds_ratio.R
@@ -6,10 +6,14 @@ require(gridExtra)
 library(RColorBrewer)
 source("/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/r_functions.R")
 
-inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/outFile.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/outFile.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/experimental_interactions/outFile.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/rbp_enrichments/outFile.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/outFile_simple.tsv"
+#inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/structure_comparison/outFile.tsv"
+inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/LncRNAGroupOddsRatio/real/structure_comparison/outFile_gencodebasic_background.tsv"
 
 dataset <- fread(inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
-
 
 # get all different categories  
 categories = unique(dataset$ExternalList)
@@ -20,4 +24,6 @@ for (i in categories){
   grid.table( dataset[dataset$ExternalList == i])
 }
 
-
+# Whole summary
+grid.newpage()
+grid.table( dataset)
