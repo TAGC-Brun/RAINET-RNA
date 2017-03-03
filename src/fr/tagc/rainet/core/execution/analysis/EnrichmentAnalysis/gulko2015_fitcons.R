f75f675c3f70bc109fa2340071639dc8617e9305
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-03-03 13:23:59 +0000

    now using lines instead of point

--- src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/gulko2015_fitcons.R
+++ src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/gulko2015_fitcons.R
@@ -9,47 +9,61 @@ require(gridExtra)
 library(RColorBrewer)
 #source("/home/diogo/workspace/tagc-rainet-RNA/src/fr/tagc/rainet/core/execution/analysis/RBPDomain/Rscripts/r_functions.R")
 
-lincRNALists = "/home/diogo/Documents/RAINET_data/lncRNA_info/gulko2013_fitCons/test/Rtest/lincRNA_plus_CDS_annotation.txt"
-gulkoOutput = "/home/diogo/Documents/RAINET_data/lncRNA_info/gulko2013_fitCons/test/Rtest/tx_outfile_linc_CDS.txt"
+rnaLists = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/Gulko2015/transcript_annotation/transcript_groups.txt"
+gulkoOutput = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/LncRNAGroupAnalysis/Gulko2015/read_gulko.out"
 
-lincRNAAnnotation <- fread(lincRNALists, stringsAsFactors = FALSE, header = TRUE, sep="\t")
+rnaAnnotation <- fread(rnaLists, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 gulkoData <- fread(gulkoOutput, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 
-mergeDataset <- merge( gulkoData, lincRNAAnnotation, by=c("transcript"))
+mergeDataset <- merge( gulkoData, rnaAnnotation, by=c("transcript"))
+
+nrow(rnaAnnotation[rnaAnnotation$group == "CDS"]) == nrow(mergeDataset[mergeDataset$group == "CDS"])/14
 
 ## If wanting to add a "other" category 
-#mergeDataset <- merge( gulkoData, lincRNAAnnotation, by=c("transcript"), all = TRUE)
+#mergeDataset <- merge( gulkoData, rnaAnnotation, by=c("transcript"), all = TRUE)
 #mergeDataset$group[is.na(mergeDataset$group)] = "other"
 
 # for each group of transcripts and for each score threshold, calculate proportion of nucleotides above cutoff
 counter = 0
+datalist = list()
 for (grp in unique(mergeDataset$group) ){
   dtGroup = mergeDataset[ mergeDataset$group == grp]
   for (scr in unique(mergeDataset$score_cutoff)){
     counter = counter + 1
     dtGroupScore = dtGroup[dtGroup$score_cutoff == scr]
-    percAbove = round(sum( dtGroupScore$ncAbove) / sum( dtGroupScore$txSize), 2)
-    datalist[[counter]] <- c(grp,scr,percAbove)
+    propAbove = round(sum( dtGroupScore$ncAbove) / sum( dtGroupScore$txSize), 2)
+    datalist[[counter]] <- c(grp,scr,propAbove)
    }
 }
 
 results = do.call(rbind.data.frame, datalist)
-colnames(results) <- c("group", "score_threshold", "proportion_above")
+colnames(results) <- c("group", "score_cutoff", "proportion_above")
+
+# need to convert to numeric otherwise they are factor
+results$score_cutoff = as.numeric(levels(results$score_cutoff)[results$score_cutoff]) 
+results$proportion_above = as.numeric(levels(results$proportion_above)[results$proportion_above])
+
+# ## Density plot with proportion per transcript
+# plt0 <- ggplot( mergeDataset[mergeDataset$score_cutoff == 0.5], aes(x = percAbove, color = group) )  +
+#   geom_density( ) +
+#   theme_minimal()
+# plt0
+
+# ## Bar plot with proportion per threshold
+# plt1 <- ggplot( results, aes(x = score_cutoff, y = proportion_above, fill = group) )  +
+#   geom_bar( stat = "identity", position = "dodge") +
+#   theme_minimal()
+# plt1
 
 
-## Bar plot with proportion per threshold
-plt1 <- ggplot( results, aes(x = score_threshold, y = proportion_above, fill = group) )  +
-  geom_bar( stat = "identity", position = "dodge") +
-  theme_minimal()
-plt1
-
-results
-
-
-## Bar plot with proportion per threshold
-plt1 <- ggplot( results, aes(x = score_threshold, y = proportion_above, color = group) )  +
+## Scatter plot with proportion per threshold
+plt2 <- ggplot( results, aes(x = score_cutoff, y = proportion_above, color = group, fill = group) )  +
   geom_point( ) +
-#  geom_line() +
+  geom_line( ) +
+  xlab("fitCons threshold (S)") +
+  ylab("Coverage of annotation type") +
+  scale_x_continuous(breaks = c(0.06,0.07)) +
   theme_minimal()
-plt1
+plt2
+
 
