fdbffdfad98d1fcdcf310520b3e115850c4a98a6
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-03-03 13:24:57 +0000

    added folder for utils related to catrapid processing

---
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/util/catrapid/catrapid_file_reformatting.py
@@ -0,0 +1,24 @@
+
+# 13-Feb-2017 Diogo Ribeiro
+
+# Simple routine to reformat from:
+# ENST00000448096|P52298  90.87   1
+# To:
+# sp|O00425|sp ENST00000470491    1
+
+import sys
+
+inputFile = sys.argv[1]
+outputFile = open( sys.argv[2], "w")
+
+with open( inputFile, "r") as inFile:
+    for line in inFile:
+        spl = line.strip().split( "\t")
+        
+        txID,protID = spl[0].split("|")
+        score = spl[1]
+        
+        outputFile.write("sp|%s|sp %s\t%s\n" % (protID, txID, score) )
+        
+outputFile.close()
+print "FINISHED!"
