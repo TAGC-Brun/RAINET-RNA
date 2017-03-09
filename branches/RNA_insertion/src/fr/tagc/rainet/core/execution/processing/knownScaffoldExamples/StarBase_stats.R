b736f7f078ded874bc41686fd3fa81ad139edb25
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-03-09 13:25:12 +0000

    structural improvements, usable as Rscript with a short version which does not produce plots

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/StarBase_stats.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/StarBase_stats.R
@@ -1,4 +1,8 @@
 
+## Diogo Ribeiro
+## Script to create a ROC curve and provide statistics on comparing catRAPID predictions against experimentally determined interactions
+## Used generally for all types of experimental datasets, NPInter, StarBase, eCLIP etc.
+
 library(ggplot2)
 library(reshape)
 require(grid)
@@ -6,18 +10,6 @@ require(gridExtra)
 library(data.table)
 library(Epi)
 library(ROCR)
-
-##### Based on NPInter_stats.R
-
-args <- commandArgs(TRUE)
-# Test if we have enough arguments
-if( length(args) != 2){
-  stop("Rscript: Bad argument number")
-}
-inputFile = args[1]
-outFolder = args[2]
-
-setwd(outFolder)
 
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/StarBasePredictionValidation/lncRNAs/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/StarBasePredictionValidation/lncRNAs/extraStringent/scores.tsv"
@@ -39,7 +31,6 @@ setwd(outFolder)
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/ensembl68_old/lncRNAs_only/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/ensembl68_old/otherRNAs_only/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/ensembl68_old/snoRNAs_only/scores.tsv"
-
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/ensembl68_old/snoRNAs_only_normalisation/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/ensembl68_old/lncRNAs_only_normalisation/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/normalised/scores.tsv"
@@ -47,13 +38,37 @@ setwd(outFolder)
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/StarBasePredictionValidation/lncRNAs/expression_dataset/lncRNAs/min100/sampled_scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/snoRNA_only/scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/new_dataset/Positive_proteins_transcripts/scores.tsv"
-
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ECLIPPredictionValidation/sampled_scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps/scores.tsv"
-inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/mRNAs/sampled_scores.tsv"
-
-
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/mRNAs/sampled_scores.tsv"
 #inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/StarBasePredictionValidation/mRNAs/3utr/extraStringent/scores.tsv"
+# 
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/scores.tsv"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/eclip_more_10_peaks/scores.tsv"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs_replicate_merge/scores.tsv"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs_replicate_merge_no_filtering/scores.tsv"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/112_rbps_3926_eCLIP_background/scores.tsv"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/112_rbps_3926_eCLIP_background/split/scores_ab"
+
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/split/scores_split_27_P52298"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/split/scores_split_41_Q13148"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/eCLIP_background_plus_functional_lncRNAs/scores_P52298_final.tsv"
+# inputFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/eCLIP_background_plus_functional_lncRNAs/RPISeq/scores.tsv"
+
+
+shortVersion = 0
+
+args <- commandArgs(TRUE)
+# Test if we have enough arguments
+if( length(args) != 3){
+  stop("Rscript: Bad argument number")
+}
+inputFile = args[1]
+outFolder = args[2]
+shortVersion = args[3]
+
+setwd(outFolder)
+
 
 dataset <- fread(inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 
@@ -64,13 +79,12 @@ dataset <- fread(inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 validated = dataset[dataset$in_validated_set == 1,]
 nonValidated = dataset[dataset$in_validated_set == 0]
 
-nrow(validated)
-nrow(nonValidated)
-
-median(validated$catrapid_score)
-median(nonValidated$catrapid_score)
-
-sd(dataset$catrapid_score)
+### Basic statistics
+paste("Number of experimental interactions:", nrow(validated))
+paste("Number of non-experimental interactions:",nrow(nonValidated))
+paste("Median score of experimental interactions:",median(validated$catrapid_score))
+paste("Median score of non-experimental interactions:",median(nonValidated$catrapid_score))
+paste("Kolmogorov-Smirnov pvalue:", ks.test(validated$catrapid_score, nonValidated$catrapid_score, alternative = c("two.sided"))$p.value)
 
 ###################################### 
 ###################################### 
@@ -78,35 +92,6 @@ sd(dataset$catrapid_score)
 ###################################### 
 ###################################### 
 
-###################################### 
-# Plot catRAPID scores distributions in and outside StarBase
-###################################### 
-
-# testing difference of distributions (Kolmogorow-Smirnov  )
-ksTestWhole = ks.test(validated$catrapid_score, nonValidated$catrapid_score, alternative = c("two.sided"))
-
-plt0 <- ggplot(dataset, aes(x=catrapid_score, colour = as.factor(in_validated_set))) + 
-  geom_density() + 
-  theme_minimal() +
-  xlab( "Score") +
-  ylab( "Density") +
-  annotate("text",  x=Inf, y = Inf, label = paste("# True: ",nrow(validated),"\n# False: ",nrow(dataset)-nrow(validated)), vjust=1, hjust=1) +
-  annotate("text",  x=Inf, y = 0, label = paste("KS test p-val: ", round(ksTestWhole$p.value,2)), vjust=1, hjust=1)
-plt0
-
-# ###################################### 
-# # Plot catRAPID scores versus CLIP reads mapped in StarBase
-# ###################################### 
-# 
-# correlation = cor(validated$clipReads,validated$catrapid_score, method = "spearman")
-# correlationSign = as.numeric(cor.test(validated$catrapid_score, validated$clipReads, method = "spearman")$p.value)
-# correlationText = paste("Corr:", round(correlation,2),"(pval:", round(correlationSign),")")
-# 
-# plt1 <- ggplot(validated, aes(x = clipReads, y = catrapid_score)) + 
-#   geom_point(shape=1) + 
-#   geom_smooth(method=lm) + 
-#   annotate("text", x = Inf, y = Inf, label = correlationText, hjust = 1, vjust =1  )
-# plt1
 
 ###################################### 
 # ROC curve 
@@ -114,14 +99,21 @@ plt0
 
 # Use ROCR
 pred = prediction(dataset$catrapid_score, dataset$in_validated_set, label.ordering = NULL)
-
 perf = performance(pred, measure = "tpr", x.measure = "fpr")
+
+auc = performance(pred, measure = "auc")
+
+paste("AUC:", round(auc@y.values[[1]],2))
+
+### Stop script if running only short version
+if (shortVersion == 1){
+  stop("short version of script ends here")
+}
+
 
 # ROC curve with ROCR package (fast)
 plot(perf, ylab = "Sensitivity", xlab = "1-Specificity", lwd= 3, main = "ROC curve")
 abline(a = 0, b = 1, col = "gray60")
-
-auc = performance(pred, measure = "auc")
 text(0.9, 0.4, paste("AUC:",round(auc@y.values[[1]],2) ) )
 
 # Optimal cutoff / cutpoint, best sensitivity and best specificity
@@ -140,10 +132,41 @@ for (cost in seq(1,3)){
   cutoff = perf@alpha.values[[1]][calc]
   sens = perf@y.values[[1]][calc]
   spec = 1-perf@x.values[[1]][calc]
-  print (paste(cost, cutoff , sens, spec) )
+#   print (paste(cost, cutoff , sens, spec) )
   points(x = 1-spec, y = sens, col = colours[cost], pch = 16)
 }
 legend("bottomright", paste("cost =", 1:3), col = colours[1:3], pch = 16, cex = 0.8, title = "Sensitivity")
+
+
+###################################### 
+# Plot catRAPID scores distributions in and outside StarBase
+###################################### 
+# 
+# # testing difference of distributions (Kolmogorow-Smirnov  )
+# ksTestWhole = ks.test(validated$catrapid_score, nonValidated$catrapid_score, alternative = c("two.sided"))
+# 
+# plt0 <- ggplot(dataset, aes(x=catrapid_score, colour = as.factor(in_validated_set))) + 
+#   geom_density() + 
+#   theme_minimal() +
+#   xlab( "Score") +
+#   ylab( "Density") +
+#   annotate("text",  x=Inf, y = Inf, label = paste("# True: ",nrow(validated),"\n# False: ",nrow(dataset)-nrow(validated)), vjust=1, hjust=1) +
+#   annotate("text",  x=Inf, y = 0, label = paste("KS test p-val: ", round(ksTestWhole$p.value,2)), vjust=1, hjust=1)
+# plt0
+
+# ###################################### 
+# # Plot catRAPID scores versus CLIP reads mapped in StarBase
+# ###################################### 
+# 
+# correlation = cor(validated$clipReads,validated$catrapid_score, method = "spearman")
+# correlationSign = as.numeric(cor.test(validated$catrapid_score, validated$clipReads, method = "spearman")$p.value)
+# correlationText = paste("Corr:", round(correlation,2),"(pval:", round(correlationSign),")")
+# 
+# plt1 <- ggplot(validated, aes(x = clipReads, y = catrapid_score)) + 
+#   geom_point(shape=1) + 
+#   geom_smooth(method=lm) + 
+#   annotate("text", x = Inf, y = Inf, label = correlationText, hjust = 1, vjust =1  )
+# plt1
 
 
 ###################################### 
