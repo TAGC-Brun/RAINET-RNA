de2280769b2ab5e6e6e549826f03c65cdb6b2523
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-03-31 14:16:14 +0000

    converted parse_enrichment_results script into a Class with an unittest

--- test/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/FilterEnrichmentResultsUnittest.py
+++ test/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/FilterEnrichmentResultsUnittest.py
@@ -28,187 +28,131 @@ class FilterEnrichmentResultsUnittest(unittest.TestCase):
 
         # Set the options
 
-        self.rainetDB = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/db_backup/RNA/rainet2016-12-28.human_lncRNA_cutoff50/rainet2016-12-28.human_lncRNA_cutoff50.sqlite"        
-        self.outputFolder = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/execution/analysis/test_output/complex_dataset_overlap/"
-        self.useInteractingProteins = 0
-        self.listDatasets = ComplexDatasetOverlap.DEFAULT_DATASET_LIST
-        self.highOverlapStat = ComplexDatasetOverlap.DEFAULT_HIGH_OVERLAP
+        self.enrichmentPerRNAFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_input/EnrichmentAnalysis/enrichment_per_rna.tsv"
+        self.enrichmentResultsFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_input/EnrichmentAnalysis/enrichment_results.tsv"
+        self.outputFolder = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_results/enrichmentAnalysis/FilterEnrichmentResults"
+        self.matrixValueColumn = 7
+        self.filterWarningColumn = 1
+        self.filterWarningValue = "1.0"
+        self.minimumRatio = "OFF"
+        self.rowAnnotationFile = ""
+        self.colAnnotationFile = ""
+        self.maskMultiple = 1
+        self.noAnnotationTag = "Other"
+        self.noAnnotationFilter = 0
+        self.annotSpecificityFilter = -1
+        self.transcriptSpecificityFilter = -1
         
-        self.run = ComplexDatasetOverlap( self.rainetDB, self.outputFolder, self.useInteractingProteins, self.listDatasets, self.highOverlapStat)
+        self.run = FilterEnrichmentResults( self.enrichmentPerRNAFile, self.enrichmentResultsFile, self.outputFolder, self.matrixValueColumn, self.filterWarningColumn, \
+                                                          self.filterWarningValue, self.minimumRatio, self.rowAnnotationFile, self.colAnnotationFile, \
+                                                          self.maskMultiple, self.noAnnotationTag, self.noAnnotationFilter, self.annotSpecificityFilter, \
+                                                          self.transcriptSpecificityFilter)
 
         
-    def test_read_rainet_db(self):
-
-        print "| test_read_rainet_db | "
+        self.expectedFolder = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_expected/enrichmentAnalysis/FilterEnrichmentResults"
         
-        self.run.read_rainet_db( )
         
-        datasetDict = self.run.datasetDict
-        
-        self.assertTrue( len( datasetDict) == 5)
+    def test_read_enrichment_per_rna_file(self):
 
-        self.assertTrue( len( datasetDict["BioplexCluster"]) == 354 )
-        
-        self.assertTrue( "Q16539" in datasetDict["BioplexCluster"]["254"])
-        
-
-    def test_read_rainet_db_two(self):
-
-        print "| test_read_rainet_db_two | "
-        
-        self.run.useInteractingProteins = 1
- 
-        self.run.read_rainet_db( )
-        
-        datasetDict = self.run.datasetDict
-
-        # complex 97b contains only two proteins that are not interacting
-
-        self.assertTrue( "97b" not in datasetDict["BioplexCluster"])
-
-        self.assertTrue( len( datasetDict["BioplexCluster"]) == 349 )
-
-
-    def test_group_overlap(self):
-
-        print "| test_group_overlap | "
-        
-        ## No overlap
-        
-        group1 = {"P1", "P2", "P3"}
-        group2 = {"P4", "P5", "P6"}
-        
-        perc1, perc2 = ComplexDatasetOverlap.group_overlap( group1, group2)
-
-        self.assertTrue( perc1 == 0.0)
-        self.assertTrue( perc2 == 0.0)
-
-        ## 1 overlap
-        
-        group1 = {"P1", "P2", "P3"}
-        group2 = {"P3", "P4", "P5", "P6",}
-        
-        perc1, perc2 = ComplexDatasetOverlap.group_overlap( group1, group2)
-
-        self.assertTrue( "%.2f" % perc1 == "33.33")
-        self.assertTrue( "%.2f" % perc2 == "25.00")
-        
-        ## full overlap
-        group1 = {"P1", "P2", "P3"}
-        group2 = {"P1", "P2", "P3"}
-        
-        perc1, perc2 = ComplexDatasetOverlap.group_overlap( group1, group2)
-
-        self.assertTrue( "%.2f" % perc1 == "100.00")
-
-
-    def test_intra_dataset_overlap(self):
-
-        print "| test_intra_dataset_overlap | "
-
-        self.run.listDatasets = ["BioplexCluster"]
-
-        self.run.read_rainet_db( )
-        
-        self.run.intra_dataset_overlap()
-        
-        self.assertTrue( len( self.run.intraDatasetOverlapResults[ "BioplexCluster"]) == len( self.run.datasetDict["BioplexCluster"]))
-
-
-        # Test: O00303 is found in two complexes, but the other proteins of complex 102a are only found in that complex
-        # "102a","O00303"
-        # "102a","Q8IUZ0"
-        # "102a","Q9H6J7"
-        # "25b","O00303"        
-
-        testingResults = self.run.intraDatasetOverlapResults[ "BioplexCluster"]["102a"] #e.g. BioplexCluster    102a    0.09    1    0
-
-        spl = testingResults.strip().split( "\t")
+        print "| test_read_enrichment_per_rna_file | "
                 
-        mean = spl[2]
-        anyOverlap = spl[3]
-        highOverlap = spl[4]
-               
-        # test mean overlap
-        expectedVal = "%.2f" % (33.33 / len( self.run.datasetDict["BioplexCluster"]) ) 
-        self.assertTrue( mean == expectedVal)
+        listRNASignificantEnrich, countRealEnrichments, countRandomEnrichments = self.run.read_enrichment_per_rna_file()
+
+        self.assertTrue( len( listRNASignificantEnrich) == 128, "assert that correct number of RNAs is read")
+        
+        self.assertTrue( countRealEnrichments == 41302, "assert that correct number of real enrichments is read")
+
+        self.assertTrue( countRandomEnrichments - 37770.9 < 0.1, "assert that correct number of random enrichments is read")
+        
+
+    def test_read_enrichment_per_rna_file_two(self):
+
+        print "| test_read_enrichment_per_rna_file_two | "
+    
+        self.run.minimumRatio = 2
+    
+        listRNASignificantEnrich, countRealEnrichments, countRandomEnrichments = self.run.read_enrichment_per_rna_file()
+
+        self.assertTrue( len( listRNASignificantEnrich) == 63, "assert correct number of significant RNAs after filter")
+        
+
+    def test_read_enrichment_results_file(self):
  
-        # test any overlap
-        self.assertTrue( anyOverlap == "1")
-        
-        # test high overlap
-        self.assertTrue( str(highOverlap) == "0")
-        
-
-        ### testing the highOverlapStat parameter, decreasing it should find other overlaps as high
-        self.run.highOverlapStat = 30
-
-        self.run.intra_dataset_overlap()
-        
-        testingResults = self.run.intraDatasetOverlapResults[ "BioplexCluster"]["102a"] #e.g. BioplexCluster    102a    0.09    1    0
-              
-        highOverlap = testingResults.strip().split( "\t")[4]
-        
-        # test high overlap
-        self.assertTrue( str(highOverlap) == "1")
-
-        
-    def test_inter_dataset_overlap(self):
-
-        print "| test_inter_dataset_overlap | "
-
-        self.run.listDatasets = ["BioplexCluster", "NetworkModule"]
-
-        self.run.read_rainet_db( )
-        
-        self.run.inter_dataset_overlap()
-        
-        self.assertTrue( len( self.run.interDatasetOverlapResults[ "BioplexCluster|NetworkModule"]) == len( self.run.datasetDict["BioplexCluster"]) )       
-        self.assertTrue( len( self.run.interDatasetOverlapResults[ "NetworkModule|BioplexCluster"]) == len( self.run.datasetDict["NetworkModule"]) )       
-
-        # Testing example  # BioplexCluster|NetworkModule    97b     0.29    4       1
-        # 
-        # Bioplex search:
-        # "97b","P28347"
-        # "97b","Q14135"
-        # 
-        # Network module search:
-        # "10","P28347"
-        # "824","P28347"
-        # 
-        # "77","Q14135"
-        # "444","Q14135"
-        # "824","Q14135"
-        # 
-        # # there are 4 network modules with overlap, one of them 100%, others with 50% 
-        
-        testingResults = self.run.interDatasetOverlapResults[ "BioplexCluster|NetworkModule"]["97b"] 
-        
-        spl = testingResults.strip().split( "\t")
-                
-        mean = spl[2]
-        anyOverlap = spl[3]
-        highOverlap = spl[4]
-               
-        # test mean overlap
-        expectedVal = "%.2f" % ( ( 50 + 50 + 50 + 100.0) / len( self.run.datasetDict["NetworkModule"]) ) 
-        self.assertTrue( mean == expectedVal)
+        print "| test_read_enrichment_results_file | "
+         
+        listRNASignificantEnrich, countRealEnrichments, countRandomEnrichments = self.run.read_enrichment_per_rna_file()
  
-        # test any overlap
-        self.assertTrue( anyOverlap == "4")
-        
-        # test high overlap
-        self.assertTrue( str(highOverlap) == "1")
-        
-        
-        
-        
-#     def test_run(self):
-#  
-#         print "| test_run | "        
-#  
-#         self.run.run()
+        dictPairs, filteredEnrichmentResults = self.run.read_enrichment_results_file( listRNASignificantEnrich, countRealEnrichments, countRandomEnrichments)
+ 
+        print len( dictPairs)
 
+        testCount = 0       
+        for line in filteredEnrichmentResults[1:]:
+            
+            spl = line.split("\t")
+            if spl[0] == "ENST00000609548":
+                testCount += 1
+            
+            self.assertTrue( spl[8] == "1")
+
+        self.assertTrue( testCount == 71)
+
+
+        # Same test but with ratio cutoff, which should exclude testing transcript
+        self.run.minimumRatio = 2
+        listRNASignificantEnrich, countRealEnrichments, countRandomEnrichments = self.run.read_enrichment_per_rna_file()
+        dictPairs, filteredEnrichmentResults = self.run.read_enrichment_results_file( listRNASignificantEnrich, countRealEnrichments, countRandomEnrichments)
+
+        testCount = 0       
+        for line in filteredEnrichmentResults[1:]:
+            spl = line.split("\t")
+            if spl[0] == "ENST00000609548":
+                testCount += 1
+        self.assertTrue( testCount == 0)
+
+        
+    def test_run(self):
+  
+        print "| test_run | "        
+  
+        self.run.minimumRatio = 2.0 
+        self.run.matrixValueColumn = 8 
+        self.run.filterWarningValue = 0
+  
+        self.run.run()
+
+        ## expected files
+
+        # check if output files are correct
+        with open(self.outputFolder + FilterEnrichmentResults.REPORT_LIST_RNA_SIGN_ENRICH, "r") as out:                
+            with open(self.expectedFolder + FilterEnrichmentResults.REPORT_LIST_RNA_SIGN_ENRICH, "r") as exp:
+                self.assertTrue(out.read() == exp.read(), "assert if report file is correct, by expected content comparison" )
+         
+        # check if output files are correct
+        with open(self.outputFolder + FilterEnrichmentResults.REPORT_FILTERED_RNA_ANNOT_RESULTS, "r") as out:                
+            with open(self.expectedFolder + FilterEnrichmentResults.REPORT_FILTERED_RNA_ANNOT_RESULTS, "r") as exp:
+                self.assertTrue(out.read() == exp.read(), "assert if report file is correct, by expected content comparison" )
+
+
+        # check if output files are correct
+        with open(self.outputFolder + FilterEnrichmentResults.REPORT_RNA_ANNOT_RESULTS_MATRIX, "r") as out:                
+            with open(self.expectedFolder + FilterEnrichmentResults.REPORT_RNA_ANNOT_RESULTS_MATRIX, "r") as exp:
+                self.assertTrue(out.read() == exp.read(), "assert if report file is correct, by expected content comparison" )
+
+        # check if output files are correct
+        with open(self.outputFolder + FilterEnrichmentResults.REPORT_SPECIFICITY_RANK, "r") as out:                
+            with open(self.expectedFolder + FilterEnrichmentResults.REPORT_SPECIFICITY_RANK, "r") as exp:
+                self.assertTrue(out.read() == exp.read(), "assert if report file is correct, by expected content comparison" )
+
+        # check if output files are correct
+        with open(self.outputFolder + FilterEnrichmentResults.REPORT_SPECIFICITY_FILTERED_RNA_ANNOT_RESULTS, "r") as out:                
+            with open(self.expectedFolder + FilterEnrichmentResults.REPORT_SPECIFICITY_FILTERED_RNA_ANNOT_RESULTS, "r") as exp:
+                self.assertTrue(out.read() == exp.read(), "assert if report file is correct, by expected content comparison" )
+
+        # check if output files are correct
+        with open(self.outputFolder + FilterEnrichmentResults.REPORT_ENRICHMENT_SUMMARY, "r") as out:                
+            with open(self.expectedFolder + FilterEnrichmentResults.REPORT_ENRICHMENT_SUMMARY, "r") as exp:
+                self.assertTrue(out.read() == exp.read(), "assert if report file is correct, by expected content comparison" )
 
 
 #     def test_extra(self):
