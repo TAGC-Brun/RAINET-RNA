d0ab15c98fe189f8e1af0aa50c5e135e4da2d178
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-04-11 10:01:54 +0000

    Now writing network between lncRNA and complexes. Restructuring of scripts

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/PrepareDiseaseNetwork.py
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/PrepareDiseaseNetwork.py
@@ -30,8 +30,7 @@ SCRIPT_NAME = "PrepareDiseaseNetwork.py"
 
 #===============================================================================
 # Processing notes:
-# 1) To separate well each complex, each protein in a complex is identified by a composed protein-complex key, so that the same protein in different complexes is interpreted differently
-# 2) Using lncRNA gene IDs instead of transcript IDs
+# 1) Network at the lncRNA-gene and Complex level (not individual transcripts nor individual proteins)
 #===============================================================================
 
 class PrepareDiseaseNetwork(object):
@@ -39,13 +38,14 @@ class PrepareDiseaseNetwork(object):
 
     NETWORK_OUTPUT_FILE = "/disease_network.tsv"
     NODE_INFO_OUTPUT_FILE = "/disease_network_node_info.tsv"
-    CLUST_N_SEE_OUTPUT_FILE = "/disease_network_clustnsee.cns"
+#     CLUST_N_SEE_OUTPUT_FILE = "/disease_network_clustnsee.cns"
 
-    def __init__(self, rainetDB, commonDiseaseFile, outputFolder):
+    def __init__(self, rainetDB, commonDiseaseFile, outputFolder, proteinShare):
 
         self.rainetDB = rainetDB
         self.commonDiseaseFile = commonDiseaseFile
         self.outputFolder = outputFolder
+        self.proteinShare = proteinShare
 
         # make output folder
         if not os.path.exists( self.outputFolder):
@@ -61,185 +61,175 @@ class PrepareDiseaseNetwork(object):
     def read_common_disease_file(self):
         
         #===============================================================================
-        # Output file, disease network
+        # Read input file
         #===============================================================================
-        # example file
-        # source node (protein or RNA) | target node (protein) | complexID (edge att) | (lncRNA) disease (edge att, if PRI)
-        # prot1 | prot2 | complex1 | NA 
-        # prot2 | prot1 | complex1 | NA
-        # lncRNA1 | prot1 | complex1 | Telangiectasia
- 
-        # Note: only want information of complexes that are scaffolded by lncRNAs
-        # Note: complexes will be represented as a clique, all proteins in a complex need to be interacting
-        # Note: lncRNA-protein interactions come from the PRI table
-        # Note: lncRNA used here is gene ID
-         
-        complexInfo = set( ) # key -> complex ID. Objective to only write info about a complex once
-        sourceNodeDisease = set() # key -> proteinID, for proteins with disease
-        sourceInteracting = set() # key -> proteinID, for RNA-interacting proteins
-        doneInteractions = set() # prot1_prot2, to avoid duplication of interactions
+        # Note: input file contains one line per lncRNA-complex-protein-disease
+        # Note: a complex can have several diseases, but an RNA only one (or vice versa), therefore a lncRNA-complex enrichment should only contain the diseases in common between the two
+        #
+        # Example format    
+        # transcriptID    GeneID  proteinID       wordsMatched    transcriptDisease       proteinDisease  complexID       complexProteins complexInteractions
+        # ['ENST00000539975', 'SNHG1', 'P00533', "'cell;lung;cancer'", 'non-small cell lung cancer', 'LUNG CANCER |  | ALVEOLAR CELL CARCINOMA, INCLUDED;; ADENOCARCINOMA OF LUNG, INCLUDED;; NONSMALL CELL LUNG CANCER, INCLUDED;; LUNG CANCER, PROTECTION AGAINST, INCLUDED', '313|NetworkModule', 'P31749,Q9Y4K3,P07900,Q13077,P00533,O15111,Q16543,A8K0R7,Q9P1W9,P21580,P04179,Q96F81,O75541,Q12933,P08238,P46778,Q92995,Q9H4A3,O14920,P63104,Q99558,Q9UN81,Q86YJ5,Q13571,P04049,P62258', 'P08238,P07900,O15111\n']
+
         proteinNodes = set() # stores protein node IDs
         rnaNodes = set() # stores RNA node IDs
-        proteinsPerComplex = {} # key -> complexID, value -> list of proteins
-         
-        networkInfo = [] # stores info that will be written in network output file
-         
-        # read file which contains one line per lncRNA-complex-protein_disease       
+        proteinsPerComplex = {} # key -> complexID, value -> set of proteins
+        diseasePerComplex = {} # key -> complexID, value -> set of diseases
+        
+        # stores info that will be written in network output file
+        networkInfo = {} # key -> lncRNA + complex composite key, value -> info for that association
+        networkInfoDisease = {} # key -> lncRNA + complex composite key, value -> diseases for that association
+        
         inFile = open( self.commonDiseaseFile, "r")
         header = inFile.readline()
         for match in inFile:
             match = match.strip()
             spl = match.split("\t")
- 
-            #e.g.: transcriptID    GeneID  proteinID       wordsMatched    transcriptDisease       proteinDisease  complexID       complexProteins complexInteractions
-            #e.g.: ['ENST00000539975', 'SNHG1', 'P00533', "'cell;lung;cancer'", 'non-small cell lung cancer', 'LUNG CANCER |  | ALVEOLAR CELL CARCINOMA, INCLUDED;; ADENOCARCINOMA OF LUNG, INCLUDED;; NONSMALL CELL LUNG CANCER, INCLUDED;; LUNG CANCER, PROTECTION AGAINST, INCLUDED', '313|NetworkModule', 'P31749,Q9Y4K3,P07900,Q13077,P00533,O15111,Q16543,A8K0R7,Q9P1W9,P21580,P04179,Q96F81,O75541,Q12933,P08238,P46778,Q92995,Q9H4A3,O14920,P63104,Q99558,Q9UN81,Q86YJ5,Q13571,P04049,P62258', 'P08238,P07900,O15111\n']
-                         
+                          
             ##########################
             # Add transcript interaction info
             ##########################
-            # lncRNA1 | prot1 | complex1 | Telangiectasia
-            # Note: add a line for each lncRNA-protein_interacting in complex
- 
-            interactingProt = spl[8].strip().split(",")
+            # lnc1 | complex1 | 5 | pri 
+            # Note: add a line for each lncRNA-complex interaction
+            # Note: if there is several diseases being shared, these will be acumulated in the 'disease' column, not in another entry
  
             complexID = spl[6]
             geneID = spl[1]
             disease = spl[4]
-             
-            for protID in interactingProt:
-                # composed key with protein and complex
-                protTag = protID + "_" + complexID
-                # add a line for each lncRNA-protein interaction
-                networkInfo.append( [geneID, protTag, complexID, disease] )
-                sourceInteracting.add( protTag)
+            interactingProt = spl[8].strip().split(",")
+            
+            compositeKey = geneID + "_" + complexID
+
+            # add basic interaction info  
+            if compositeKey not in networkInfo:
+                networkInfo[ compositeKey] = []
+            networkInfo[ compositeKey] = [ geneID, complexID, str( len( interactingProt)), "pri"]
+
+            # add disease association info to edges
+            if compositeKey not in networkInfoDisease:
+                networkInfoDisease[ compositeKey] = set()
+            networkInfoDisease[ compositeKey].add( disease)
+
+            # add proteins to complex structure
+            if complexID not in proteinsPerComplex:
+                proteinsPerComplex[ complexID] = set()
+                complexProteins = spl[ 7].strip().split(",")
+
+                for protein in complexProteins:
+                    proteinsPerComplex[ complexID].add( protein)
+                    proteinNodes.add( protein)
+
+            # add disease information to complexes
+            if complexID not in diseasePerComplex:
+                diseasePerComplex[ complexID] = set()
+            diseasePerComplex[ complexID].add( disease)
             
             rnaNodes.add( geneID)
-            
-            diseaseProt = spl[2] + "_" + complexID
-            sourceNodeDisease.add( diseaseProt)
+
+        assert len( networkInfo) == len( networkInfoDisease)
              
-            ##########################
-            # Add protein complex interaction info (clique)
-            ##########################           
-            # prot1 | prot2 | complex1 | NA
- 
-            # only write complex interactions once for each complex            
-            if complexID not in complexInfo:
-                # get all proteins of complex
-                complexProteins = spl[ 7].strip().split(",")
-                 
-                # for each pair of proteins, write interaction (one-way)
-                for prot1 in complexProteins:
-                    
-                    # add proteins to complex structure
-                    if complexID not in proteinsPerComplex:
-                        proteinsPerComplex[ complexID] = []
-                    proteinsPerComplex[ complexID].append( prot1 + "_" + complexID)
-                    
-                    for prot2 in complexProteins:
-                        if prot1 != prot2: # avoid self interaction
+        print "read_common_disease_file : number of complex nodes: %s " % len( proteinsPerComplex)
+        print "read_common_disease_file : total number of proteins: %s " % len( proteinNodes)
+        print "read_common_disease_file : number of RNA nodes: %s " % len( rnaNodes)
+        print "read_common_disease_file : number of RNA-complex interactions (edges): %s " % len( networkInfo)
 
-                            # composite key
-                            prot1Tag = prot1 + "_" + complexID
-                            prot2Tag = prot2 + "_" + complexID
-                            
-                            if prot1 + "_" + prot2 not in doneInteractions: 
-                                networkInfo.append( [prot1Tag, prot2Tag, complexID, "NA"] )                                
-                                proteinNodes.add( prot1Tag)
-                                proteinNodes.add( prot2Tag)
-                            
-                            # avoid repetition of inverse interaction
-                            doneInteractions.add( prot1 + "_" + prot2)
-                            doneInteractions.add( prot2 + "_" + prot1)
-                         
-            complexInfo.add( complexID)
+
+
+        #===============================================================================
+        # Output file, disease network
+        #===============================================================================
+        # example file
+        # source node (complex or RNA) | target node (complex) | number partners | interaction type | diseases
+        # lnc1 | complex1 | 5 | pri | breast cancer, skin cancer
+        # complex1 | complex2 | 2 | protein share | NA
  
-        print len( proteinsPerComplex)
+        # Note: only want information of complexes that are scaffolded by lncRNAs
+        # Note: lncRNA used here is gene ID
  
-        print len( networkInfo)
- 
-        # Write network output file
+        # Write PRI
         outFile = open( self.outputFolder + PrepareDiseaseNetwork.NETWORK_OUTPUT_FILE, "w")
-        outFile.write( "Source_node\tTarget_node\tComplexID\tDisease_association\n")
-        for line in networkInfo:
-            outFile.write( "\t".join( line) + "\n")
-                         
+        outFile.write( "Source_node\tTarget_node\tNumber partners\tInteraction type\tDisease associations\n")
+        for assoc in sorted( networkInfo):
+            # add disease info to network line
+            diseaseMerge = ", ".join( networkInfoDisease[ assoc])
+            networkInfo[ assoc].append( diseaseMerge )
+            outFile.write( "\t".join( networkInfo[ assoc]) + "\n")
+
+        # Write complex protein share
+        # complex1 | complex2 | 2 | protein share | NA
+
+        complexShare = {}
+        if self.proteinShare != -1:
+            complexShare = self.calculate_protein_share(proteinsPerComplex)
+            
+            for pair in complexShare:
+                comp1, comp2 = pair.split( "_")
+                outFile.write( "%s\t%s\t%i\tprotein share\tNA\n" % ( comp1, comp2, complexShare[ pair] ) )
+                    
         outFile.close()
- 
-        print "read_common_disease_file: %s disease-associated complexes." %  len( complexInfo)
-        # TODO: correct this, because does not work with composite keys
-#        print "read_common_disease_file: %s disease-associated proteins." %  len( sourceNodeDisease)
-
-
+        
         #===============================================================================
         # Output file, node info table
         #===============================================================================
-
+        # Node ID | Node type | Complex size (if applicable) | Complex Disease
+ 
         outFile2 = open(self.outputFolder + PrepareDiseaseNetwork.NODE_INFO_OUTPUT_FILE, "w")
-        outFile2.write("Node\tType\tDisplay name\tDisease node\tRNA-interacting protein\n")
-
+        outFile2.write("Node\tType\tComplex size\tComplex disease\n")
+ 
         # Write info of RNA nodes
         for rna in rnaNodes:
-            outFile2.write("%s\tLncRNA\%s\t1\t1\n" % ( rna, rna) )
-
-        for prot in proteinNodes:
-            # tag disease proteins
-            isDisease = 0
-            if prot in sourceNodeDisease:
-                isDisease = 1
-                
-            # tag interacting proteins
-            isInteracting = 0
-            if prot in sourceInteracting:
-                isInteracting = 1
-
-            # TODO: write protein name instead of uniprotAC
-            outFile2.write("%s\tProtein\t%s\t%s\t%s\n" % ( prot, prot, isDisease, isInteracting ) )
-
+            outFile2.write( "%s\tLncRNA\tNA\tNA\n" % ( rna) )
+ 
+        # Write info for complex nodes
+        for comp in proteinsPerComplex:
+            outFile2.write( "%s\tComplex\t%i\t%s\n" % ( comp, len( proteinsPerComplex[ comp]), ", ".join( diseasePerComplex[ comp]) ) )
+ 
         outFile2.close()
 
+        self.networkInfo = networkInfo
+        self.complexShare = complexShare
 
+    # #
+    # Calculate number of proteins shared between all combinations of two complexes (one-way, non-self)
+    def calculate_protein_share(self, proteinsPerComplex):
+        
         #===============================================================================
-        # Output file, clust N see partitions
+        # Calculate complex protein share
         #===============================================================================
-        # Note: each lncRNA in its own partition
-        # Note: each complex in its own partition
-        # Note: all nodes in partition needs also to be present in network
+        # Note: complex interactions are number of proteins being shared between them
+        # Note: only write interactions where there is at least one protein being shared
 
-        # Example format      
-        #         # ClustnSee analysis export
-        #
-        #         >ClusterID:1||
-        #         prot1
-        #
-        #         >ClusterID:1||
-        #         prot1
-        #         prot2
+        complexShare = {} # key -> e.g. complex1_complex2, value -> number proteins being shared
+    
+        donePairs = set() # store done comparisons, so that we only do them one way
+        for comp1 in proteinsPerComplex:
+            for comp2 in proteinsPerComplex:
+                if comp1 != comp2:
+                    
+                    tag1 = comp1 + "_" + comp2
+                    tag2 = comp2 + "_" + comp1
 
-        outFile3 = open( self.outputFolder + PrepareDiseaseNetwork.CLUST_N_SEE_OUTPUT_FILE, "w")
-        outFile3.write( "#ClustnSee analysis export\n\n")
+                    if tag1 not in donePairs:
+                        overlap = len( proteinsPerComplex[ comp1].intersection( proteinsPerComplex[ comp2]) )
 
-        # keep track of cluster ID, an incremental number
-        clustID = 1
+                        if overlap >= self.proteinShare:
+                            complexShare[ tag1] = overlap
+                    
+                    donePairs.add( tag1)
+                    donePairs.add( tag2)
+        
+        possibleCombinations = ((len( proteinsPerComplex)-1) * len( proteinsPerComplex)) / 2
+        assert( len( complexShare) <= possibleCombinations)
+        
+        print "calculate_protein_share : number of complex combinations sharing proteins: %s, out of %s possible combinations" % ( len( complexShare), possibleCombinations )
+        
 
-        # write lncRNA partitions
-        for rna in rnaNodes:
-            outFile3.write( ">ClusterID:%i||\n" % clustID)
-            outFile3.write( rna + "\n")        
-            outFile3.write("\n")
-            clustID += 1
+        return complexShare
 
-        # write complex partitions
-        for comp in proteinsPerComplex:
-            outFile3.write( ">ClusterID:%i||\n" % (clustID) )
-            for prot in proteinsPerComplex[ comp]:
-                outFile3.write( prot + "\n")                        
-            outFile3.write("\n")
-            clustID += 1
 
-        outFile3.close()
+    def run(self):
+        
+        self.read_common_disease_file()
 
-        # TODO: unittest: ensure that at least one protein in each complex has disease
 
 
 if __name__ == "__main__":
@@ -263,15 +253,17 @@ if __name__ == "__main__":
                              help='File with matches between lncRNA-complex diseases, the manually curated output from CommonLncRNAProteinDisease.py.')
         parser.add_argument('outputFolder', metavar='outputFolder', type=str,
                              help='Output folder.')
+        parser.add_argument('--proteinShare', metavar='--proteinShare', type=int, default = -1,
+                             help='Whether to add interactions for protein share between complexes, and how many minimum proteins needed for writing interaction. Default = -1 (OFF)')
            
         # gets the arguments
         args = parser.parse_args( ) 
     
         # Initialise class
-        instance = PrepareDiseaseNetwork( args.rainetDB, args.commonDiseaseFile, args.outputFolder)
+        instance = PrepareDiseaseNetwork( args.rainetDB, args.commonDiseaseFile, args.outputFolder, args.proteinShare)           
             
         instance.run()
-            
+        
         # Stop the chrono      
         Timer.get_instance().stop_chrono( "FINISHED " + SCRIPT_NAME )
 
--- branches/RNA_insertion/test/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/PrepareDiseaseNetworkUnittest.py
+++ branches/RNA_insertion/test/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/PrepareDiseaseNetworkUnittest.py
@@ -20,11 +20,12 @@ class PrepareDiseaseNetworkUnittest(unittest.TestCase):
         self.rainetDB = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/db_backup/RNA/rainet2016-12-28.human_lncRNA_cutoff50/rainet2016-12-28.human_lncRNA_cutoff50.sqlite"
         self.commonDiseaseFile = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_input/EnrichmentAnalysis/PrepareDiseaseNetwork/manually_curated_common_disease.txt"
         self.outputFolder = "/home/diogo/workspace/tagc-rainet-RNA/test/fr/tagc/rainet/core/test_results/enrichmentAnalysis/PrepareDiseaseNetwork"
+        self.proteinShare = -1
 
         # folder containing expected output files
         self.expectedFolder = "test_expected/"
         
-        self.run = PrepareDiseaseNetwork(self.rainetDB, self.commonDiseaseFile, self.outputFolder )
+        self.run = PrepareDiseaseNetwork(self.rainetDB, self.commonDiseaseFile, self.outputFolder, self.proteinShare )
             
 
     # #
@@ -34,16 +35,56 @@ class PrepareDiseaseNetworkUnittest(unittest.TestCase):
 
         self.run.read_common_disease_file()
 
+        ## Test networkInfo variable
+        
+        netInfo = self.run.networkInfo
+            
+        #cut -f2,7 manually_curated_common_disease.txt| tail -n +2 | sort -u | wc -l  66
+        self.assertTrue( len( netInfo) == 66)
+        
+        #ENST00000522875    PVT1    P50539    'prostate;cancer'    prostate cancer    PROSTATE CANCER |  |    77|NetworkModule    Q96SQ5,P50151,Q96M91,Q5TAP6,Q8N6Y0,Q8IUQ4,P40222,P52292,Q6NYC8,Q9GZM8,A0A0C4DH19,P15622,Q70UQ0,I3L4J3,Q6XQN6,O15066,H3BTW2,Q9H741,O75386,P50539,Q9NQ69,Q96CN9,Q9HAT0,Q8NF50,Q93062,Q8TD31,Q01850,O76015,Q96GY3,Q86WW8,Q6PCT2,Q96MT8,Q08AG5,Q68D86,Q96BZ8,Q9H1X1,Q8N2I2,Q9P2K3,Q9NX04,P63000,Q9NS73,P08670,P35900,O75069,Q9BVG8,Q96GV9,Q86YD7,A8K8P3,Q92552,Q86YD1,Q6P1W5,Q9Y2J4,Q8ND90,Q9NYB9,Q6ZU52,Q8TCX5,Q8N0S2,O60763,P14136,Q9H257,Q13077,Q9Y3B7,Q9BZW7,P25786,Q9BYV2,O15015,P43356,Q96EV8,Q9H6J7,Q8IXL7,Q9NP87,O60341,O75604,P51116,P24863,Q7Z3B3,Q2KHM9,Q9H788,Q5JR59,Q96CS2,Q92620,Q8N8B7,Q8WWY3,P14373,O60293,Q3B820,Q08117,Q9UBB9,P07196,P13762,Q9P0W2,Q6A162,Q86Y26,P50053,Q8IYE0,Q8NA61,Q53EZ4,Q8N1B4,Q5T5P2,Q9UIA0,Q8N5R6,Q05516,O95671,P06753,Q9Y2D8,Q9H875,O94972,P19012,Q8IWZ5,P63218,Q14687,Q14D33,Q6NX49,Q9BWJ2,Q86Y33,P41219,Q5VU43,Q9BXY8,Q9Y6D9,O75558,Q9P126,Q8IYX8,Q04695,Q8IUH5,Q92917,Q7Z6G3,P55081,Q9P0T4,Q9ULM2,Q9BUI4,Q9UPU9,Q96PV4,O43482,P36406,P12036,O00560,Q08379,Q07002,Q13976,Q9UJV3,Q5GH77,Q8IYF3,Q8N3L3,Q9Y4E8,P98175,Q96EA4,Q15323,P25791,Q13137,Q15654,Q14134,Q14135,Q8N720,Q8WUT1,Q8IY82,P48668,Q99996,O95990,P02538,Q9H2F5,O43347,Q9BRK4,Q8NDB6,P08727,Q5W0B1,Q15834,Q9Y2I6,Q96HB5,Q2TBE0,Q96I25,Q6IPM2,Q8TD10,Q2TAC2,Q8WVF2,Q15561,P43360,Q9NU19,P43364,Q12933,Q9H0W8,Q14129,Q8NHQ1,Q16512,Q99750,Q92752,Q9Y247,Q9HBZ2    O75069,Q6NX49,Q5W0B1,Q8N6Y0,Q8N1B4,Q6IPM2,Q8N3L3,Q9Y6D9,Q9HBZ2,O15066,Q96MT8
+
+        self.assertTrue( netInfo[ "PVT1_77|NetworkModule"][0] == "PVT1")
+        self.assertTrue( netInfo[ "PVT1_77|NetworkModule"][1] == "77|NetworkModule")
+        self.assertTrue( netInfo[ "PVT1_77|NetworkModule"][2] == str(11))
+        self.assertTrue( netInfo[ "PVT1_77|NetworkModule"][4] == "prostate cancer")
+
+        # PVT1 vs 350|NetworkModule has several disease associations
+
+        self.assertTrue( len(netInfo[ "PVT1_350|NetworkModule"][4].split(",")) == 7)
 
 
-# 
-#     def test_run(self):
-# 
-#         print "| test_run | "
-#         
-#         self.run.run()
-#         
-# 
+
+    # #
+    def test_calculate_protein_share(self):
+
+        print "| test_calculate_protein_share | "
+
+        self.run.proteinShare = 1
+
+        proteinsPerComplex = { "comp1" : set( ["p1", "p2", "p3"]), "comp2" : set( ["p3", "p5"]), "comp3" : set( ["p6"])}
+
+        ## Test complexShare variable
+        
+        complexShare = self.run.calculate_protein_share(proteinsPerComplex)
+        
+        self.assertTrue( len( complexShare) == 1)
+
+
+        self.run.proteinShare = 0
+        self.run.read_common_disease_file()
+        self.assertTrue( len( self.run.complexShare) == 50 * 51 / 2 )
+        self.assertTrue( self.run.complexShare["28|NetworkModule_773|NetworkModule"] == 0)
+        self.assertTrue( self.run.complexShare["79|NetworkModule_104|NetworkModule"] == 26)
+
+ 
+    def test_run(self):
+ 
+        print "| test_run | "
+         
+        self.run.run()
+         
+ 
 #     # #
 #     # Runs after each test
 #     def tearDown(self):
