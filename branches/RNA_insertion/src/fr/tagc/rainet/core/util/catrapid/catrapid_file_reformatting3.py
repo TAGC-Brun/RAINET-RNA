94ec5998353341b40fb78155763a2e67693ebaec
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-05-02 13:23:32 +0000

    small routine to reformat catrapid format into simple tsv

---
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/util/catrapid/catrapid_file_reformatting3.py
@@ -0,0 +1,27 @@
+
+# 2-May-2017 Diogo Ribeiro
+
+# Simple routine to reformat from:
+# sp|Q53F19|NCBP3_HUMAN ENST00000423712   -58.53  0.00    0.00
+# To:
+# ENST00000423712    Q53F19    -58.53
+
+import sys
+
+inputFile = sys.argv[1]
+outputFile = open( sys.argv[2], "w")
+
+with open( inputFile, "r") as inFile:
+    for line in inFile:
+        spl = line.strip().split( "\t")
+        
+        ids = spl[0]
+        prot, txID = ids.split(" ")
+        score = spl[1]
+
+        protID = prot.split("|")[1]
+        
+        outputFile.write("%s\t%s\t%s\n" % (txID, protID, score) )
+        
+outputFile.close()
+print "FINISHED!"
