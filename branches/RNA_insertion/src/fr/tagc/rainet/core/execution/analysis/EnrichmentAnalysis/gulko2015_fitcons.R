a8c17c392ed42684c43ed0b1537355beb39af851
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-05-03 16:32:09 +0000

    now plotting error bars after subsampling

--- branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/gulko2015_fitcons.R
+++ branches/RNA_insertion/src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/gulko2015_fitcons.R
@@ -16,6 +16,9 @@ gulkoOutput = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/Lnc
 rnaAnnotation <- fread(rnaLists, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 gulkoData <- fread(gulkoOutput, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 
+# To remove CDS from analysis
+rnaAnnotation = rnaAnnotation[ rnaAnnotation$group != "CDS"]
+
 mergeDataset <- merge( gulkoData, rnaAnnotation, by=c("transcript"))
 
 # nrow(rnaAnnotation[rnaAnnotation$group == "CDS"]) == nrow(mergeDataset[mergeDataset$group == "CDS"])/14
@@ -66,15 +69,13 @@ plt2 <- ggplot( results, aes(x = score_cutoff, y = proportion_above, color = gro
   ylab("Sequence covered") +
 #  scale_x_continuous(breaks = c(0.06, 0.07, 0.08, 0.09, 0.10, 0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80)) +
   scale_x_continuous(breaks = seq(from = 0, to = 0.8, by = 0.05))  +
-  scale_colour_brewer(palette = "Set1", labels = c("3'UTR","5'UTR","CDS","Scaffolding lincRNAs","Other lincRNAs")) +
+  scale_colour_brewer(palette = "Set1", labels = c("3'UTR","5'UTR","Scaffolding lincRNAs","Other lincRNAs")) + #labels = c("3'UTR","5'UTR","CDS","Scaffolding lincRNAs","Other lincRNAs")) 
   labs(color = "") +
   #  scale_color_manual(values = simple_color_palette) +
   theme_bw() + 
   theme(legend.position="right", legend.direction= "vertical", legend.key = NULL) + 
   theme(text = element_text(size=25))
 plt2
-
-#print as 25.91 vs 5.98 inches
 
 
 #########################
@@ -83,13 +84,16 @@ plt2
 # Subsampling groups
 
 nsample = 50
-nrandom = 3
+nrandom = 100
 
+# container of values after each randomisation
+deviations = data.frame()
+
+# For each randomisation
 for (i in 1:nrandom){
 
+  # store features to be picked for each randomisation
   sampleRnaAnnotation = data.frame()
-  
-  #results = results[results$group != "CDS",]
   
   # For each group of transcripts, pick the sample amount of transcripts
   for (grp in unique(rnaAnnotation$group) ){
@@ -100,7 +104,8 @@ for (i in 1:nrandom){
   
   # confirm if sampling worked
   nrow(sampleRnaAnnotation) == length(unique(rnaAnnotation$group)) * nsample
-  
+
+  # attribute gulko data to the sample transcripts  
   mergeDataset <- merge( gulkoData, sampleRnaAnnotation, by=c("transcript"))
   
   # for each group of transcripts and for each score threshold, calculate proportion of nucleotides above cutoff
@@ -113,31 +118,34 @@ for (i in 1:nrandom){
       dtGroupScore = dtGroup[dtGroup$score_cutoff == scr]
       propAbove = round(sum( dtGroupScore$ncAbove) / sum( dtGroupScore$txSize), 2)
       datalist[[counter]] <- c(grp,scr,propAbove)
+      deviations = rbind(deviations, data.frame(i, grp, scr, propAbove))
     }
   }
-  
-  results = do.call(rbind.data.frame, datalist)
-  colnames(results) <- c("group", "score_cutoff", "proportion_above")
-  
-  # need to convert to numeric otherwise they are factor
-  results$score_cutoff = as.numeric(levels(results$score_cutoff)[results$score_cutoff]) 
-  results$proportion_above = as.numeric(levels(results$proportion_above)[results$proportion_above])
-  
-  print (paste("RAND", i))
-  print (results)
-  
-  # add a new line to plot
-  plt2 = plt2 + geom_line( data = results, linetype = 2, size = 0.5, aes(x = score_cutoff, y = proportion_above, color = group))
+
+#   # add a new line to plot
+#   plt2 = plt2 + geom_line( data = results, linetype = 2, size = 0.5, aes(x = score_cutoff, y = proportion_above, color = group))
   
 }
 
+# calculate standard deviation for each set of transcript group + cutoff
+colnames(deviations) = c("rand","group","score_cutoff","proportion_above")
+calcDeviations = aggregate(deviations$proportion_above, list(deviations$group, deviations$score_cutoff), sd)
+nrow( calcDeviations) == 76 * 4
+
+colnames(calcDeviations) = c("group","score_cutoff","sd")
+head(calcDeviations)
+
+resultsPlusDev = merge(results, calcDeviations, by = c("group","score_cutoff"))
+
+# # Transform into standard error of the mean
+# resultsPlusDev$se = resultsPlusDev$sd / sqrt(nrandom)
+
+# coordinates for error bars
+resultsPlusDev$lower = resultsPlusDev$proportion_above - resultsPlusDev$sd
+resultsPlusDev$upper = resultsPlusDev$proportion_above + resultsPlusDev$sd 
+
+# Add the error bars to the plot 
+plt2 = plt2 + geom_errorbar(data = resultsPlusDev, aes(x = score_cutoff, ymin= lower, ymax= upper), colour="black", width=.01, size = 0.1)
 plt2
 
-
-# I could also calculate the standard deviation and then standard error of the mean, plot it instead of extra lines
-# E.g.
-# results$sd = results$proportion_above / 100.0 # to be replaced with actual calculation
-# 
-# plt2 = plt2 + geom_errorbar(data = results, aes(ymin=results$proportion_above - sd, ymax=results$proportion_above + sd), colour="black", width=.1)
-# plt2
-
+#print as 25.91 vs 5.98 inches
