a7008584be942e77f9f4e0a4feb724bfdbae38db
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-05-09 14:43:05 +0000

    plot and commentary improvements

--- src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/catrapid_expression_roc.R
+++ src/fr/tagc/rainet/core/execution/processing/knownScaffoldExamples/catrapid_expression_roc.R
@@ -17,16 +17,21 @@ library(ROCR)
 
 # catRAPID file has scores and also whether interaction is experimental or not
 # catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps/score_reformatted.tsv"
-catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/scores_reformatted3.tsv"
+# catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/scores_reformatted3.tsv"
 # catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/eclip_read_count/eclip_HepG2_positives_negatives.out"
 # catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/eclip_read_count/eclip_K562_positives_negatives.out"
-# catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/allInteractions/scores_reformatted3.tsv"
+catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/NPInterPredictionValidation/allInteractions/scores_reformatted3.tsv"
+# catRAPIDfile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/StarBasePredictionValidation/lncRNAs/scores_reformatted3.tsv"
 # expression file only has expression data per interaction
-expressionFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/lncrna/expression_data/eCLIP_interactions/storedInteractions_reformatted.tsv"
-# expressionFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/lncrna/expression_data/NPInter_interactions_ensembl68/storedInteractions_reformatted.tsv"
+# expressionFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/lncrna/expression_data/eCLIP_interactions/storedInteractions_reformatted.tsv"
+expressionFile = "/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/ReadCatrapid/Ensembl82/lncrna/expression_data/NPInter_interactions_ensembl68/storedInteractions_reformatted.tsv"
 
 catRAPIDData = fread(catRAPIDfile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 expressionData = fread(expressionFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
+
+###################################### 
+# Prepare input data
+###################################### 
 
 # # catrapid scores only
 # mergedData = catRAPIDData
@@ -40,17 +45,18 @@ expressionData = fread(expressionFile, stringsAsFactors = FALSE, header = TRUE, 
 # mergedData = merge(x = catRAPIDData, y = expressionData, by = c("transcript","protein"))
 # write.table(mergedData, file='/home/diogo/Documents/RAINET_data/TAGC/rainetDatabase/results/eCLIPPredictionValidation/ROC/only_112_rbps_15230_gencode_basic_lncRNAs/scores_plus_expression.tsv', quote=FALSE, sep='\t', col.names = T, row.names = F)
 
-# # Doing a left merge
+# # Doing a left merge, keeping all catRAPID interactions
 mergedData = merge(x = catRAPIDData, y = expressionData, by = c("transcript","protein"), all.x = TRUE)
+# Adding 0 to expression value when it would be NA
 mergedData[is.na(mergedData)] = 0
-
-# Apply [0,1] normalisation
-mergedData$catrapid_score = (mergedData$catrapid_score-min(mergedData$catrapid_score))/(max(mergedData$catrapid_score)-min(mergedData$catrapid_score))
-mergedData$expression_score = (mergedData$expression_score-min(mergedData$expression_score))/(max(mergedData$expression_score)-min(mergedData$expression_score))
 
 # # Apply log transformation
 # mergedData$expression_score = mergedData$expression_score + 1
 # mergedData$expression_score = log10(mergedData$expression_score)
+
+# Apply [0,1] normalisation
+mergedData$catrapid_score = (mergedData$catrapid_score-min(mergedData$catrapid_score))/(max(mergedData$catrapid_score)-min(mergedData$catrapid_score))
+mergedData$expression_score = (mergedData$expression_score-min(mergedData$expression_score))/(max(mergedData$expression_score)-min(mergedData$expression_score))
 
 # summary(mergedData$expression_score)
 # plt1 = ggplot(mergedData, aes( x = expression_score)) +
@@ -67,18 +73,20 @@ mergedData$expression_score = (mergedData$expression_score-min(mergedData$expres
 validated = mergedData[mergedData$in_validated_set == 1,]
 nonValidated = mergedData[mergedData$in_validated_set == 0]
 
-### Subsample the negatives
+### Subsample the negatives, to the same number as positives
 nonValidated = nonValidated[sample(nrow(nonValidated), nrow(validated))]
 mergedData = rbind(validated, nonValidated)
 
+####################
 ### Basic statistics
+####################
 paste("Number of experimental interactions:", nrow(validated))
 paste("Number of non-experimental interactions:",nrow(nonValidated))
+# On catRAPID scores
 paste("Median score of experimental interactions:",median(validated$catrapid_score))
 paste("Median score of non-experimental interactions:",median(nonValidated$catrapid_score))
 paste("Kolmogorov-Smirnov pvalue:", ks.test(validated$catrapid_score, nonValidated$catrapid_score, alternative = c("two.sided"))$p.value)
-
-### Basic statistics
+# On expression
 paste("Median score of experimental interactions:",median(validated$expression_score))
 paste("Median score of non-experimental interactions:",median(nonValidated$expression_score))
 paste("Kolmogorov-Smirnov pvalue:", ks.test(validated$expression_score, nonValidated$expression_score, alternative = c("two.sided"))$p.value)
@@ -88,11 +96,9 @@ paste("Kolmogorov-Smirnov pvalue:", ks.test(validated$expression_score, nonValid
 # ROC curve 
 ###################################### 
 
-betaMin = 0
-betaMax = 1000
-betaStep = 100
+betas = c(0,0.001,0.01,0.1,1,10,100,1000)
 
-for (beta in seq(betaMin, betaMax, by = betaStep)){
+for (beta in betas){
 
   # Define the metric to be used for ROC curve
   mergedData$metric = mergedData$catrapid_score + mergedData$expression_score * beta
@@ -108,39 +114,44 @@ for (beta in seq(betaMin, betaMax, by = betaStep)){
 }
 
 
-# composite model
-beta = 80
+#########################################
+# Drawing ROC curve
+#########################################
+
+##################
+# composite catrapid+expression model
+##################
+beta = 200
 mergedData$metric = mergedData$catrapid_score + mergedData$expression_score * beta
 
 # Use ROCR
 pred = prediction(mergedData$metric, mergedData$in_validated_set, label.ordering = NULL)
 perf = performance(pred, measure = "tpr", x.measure = "fpr")
-
 auc = performance(pred, measure = "auc")
 
 # ROC curve with ROCR package (fast)
-plot(perf, ylab = "Sensitivity", xlab = "1-Specificity", lwd= 3, main = "ROC curve")
-abline(a = 0, b = 1, col = "gray60")
-text(0.9, 0.4, paste("AUC:",round(auc@y.values[[1]],2) ) )
+plot(perf, ylab = "Sensitivity", xlab = "1-Specificity", lwd= 3, col = "dark green")
+abline(a = 0, b = 1, lty = 5, col = "gray60")
 
-# Optimal cutoff / cutpoint, best sensitivity and best specificity
-cutoff = perf@alpha.values[[1]][which.max(1-perf@x.values[[1]] + perf@y.values[[1]])] 
-text(0.8,0.45, paste("Max(sens+spec):",round(cutoff,2) ) )
+text(0.5, 0.9, paste("AUC:",round(auc@y.values[[1]],2) ) )
 
-text(0.16,0.90, paste("# True:",nrow(validated) ) )
-text(0.2,0.85, paste("# False:",nrow(nonValidated) ) )
+##################
+# catrapid only model
+##################
 
-colours = rainbow(3)
+beta = 0
+mergedData$metric = mergedData$catrapid_score + mergedData$expression_score * beta
 
-# Cutoff cost analysis. Augmenting weight of sensitivity
-print ("cost cutoff sens 1-spec")
-for (cost in seq(1,3)){
-  calc = which.max(1-perf@x.values[[1]] + cost*perf@y.values[[1]])
-  cutoff = perf@alpha.values[[1]][calc]
-  sens = perf@y.values[[1]][calc]
-  spec = 1-perf@x.values[[1]][calc]
-  #   print (paste(cost, cutoff , sens, spec) )
-  points(x = 1-spec, y = sens, col = colours[cost], pch = 16)
-}
-legend("bottomright", paste("cost =", 1:3), col = colours[1:3], pch = 16, cex = 0.8, title = "Sensitivity")
+pred = prediction(mergedData$metric, mergedData$in_validated_set, label.ordering = NULL)
+perf = performance(pred, measure = "tpr", x.measure = "fpr")
+auc = performance(pred, measure = "auc")
+
+lines(perf@x.values[[1]], perf@y.values[[1]], lwd = 3, lty = 2, col = "brown")
+
+text(0.5, 0.65, paste("AUC:",round(auc@y.values[[1]],2) ) )
+
+text(0.15,0.90, paste("# Positives:",nrow(validated) ) )
+text(0.16,0.85, paste("# Negatives:",nrow(nonValidated) ) )
+
+legend(0.4, 0.2, legend = c("catRAPID + expression","catRAPID only"), col = c("dark green","brown"), lty=1:2, cex=0.9)
 
