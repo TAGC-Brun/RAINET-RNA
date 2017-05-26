488bf12d7717eabd7a5011a6ae92431c688931de
diogo_ribeiro <diogo_ribeiro@bd239c00-765f-4708-9f37-acd49598262a>
2017-05-26 14:11:59 +0000

    adapted to plot data for Network Modules

--- src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/go_term_pie_plot.R
+++ src/fr/tagc/rainet/core/execution/analysis/EnrichmentAnalysis/go_term_pie_plot.R
@@ -1,6 +1,6 @@
 
 ## 11-May-2016 Diogo Ribeiro
-## Script to a pie plot with GO terms distribution on enriched complexes
+## Script to create a pie plot with GO terms distribution on enriched complexes
 
 library(data.table)
 library(plyr)
@@ -9,16 +9,23 @@ require(grid)
 require(gridExtra)
 library(RColorBrewer)
 
-inputFile = "/home/diogo/Documents/RAINET_data/macromolecular_complex_datasets/Wan2015/proportion_wan_go_term_annotations_biological_process_1.txt"
+# Wan 2015
+# inputFile = "/home/diogo/Documents/RAINET_data/macromolecular_complex_datasets/Wan2015/proportion_wan_go_term_annotations_biological_process_1.txt"
+
+# Network Modules
+inputFile = "/home/diogo/Documents/RAINET_data/macromolecular_complex_datasets/NetworkModules/proportion_wan_go_term_annotations_biological_process_1.txt"
 
 data <- fread(inputFile, stringsAsFactors = FALSE, header = TRUE, sep="\t")
 
 
 data$PERC = round(data$PROPORTION * 100.0,1)
 
-# cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#CC6666", "#9999CC", "#66CC99", "black")
-cbPalette <- c("#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#ffed6f","#ccebc5","#bc80bd")
-# cbPalette <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928")
+# # Colors for Wan 2015
+# cbPalette <- c("#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#ffed6f","#ccebc5","#bc80bd")
+
+# Colors for Network modules (to match Wan 2015 when possible)
+cbPalette <- c("#ffffb3","#bebada","#fb8072","#80b1d3", "#4eb3d3","#fdb462","#b3de69","#d9d9d9","#ffed6f","#ccebc5","#bc80bd")
+
 
 blank_theme <- theme_minimal()+
   theme(
@@ -29,7 +36,6 @@ blank_theme <- theme_minimal()+
     axis.ticks = element_blank(),
     plot.title=element_text(size=14, face="bold")
 )
-
 
 plt1 <- ggplot( data, aes(x = "", y = PROPORTION, fill = GO_NAME) )  +
   geom_bar( width = 1, color = "black", size = 0.7, alpha = 0.9) + #stat = "identity"
